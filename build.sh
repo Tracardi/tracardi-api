3ee9332cecce19b890cd3601d692e5eba38a9f0b
risto <risto.kowaczewski@gmail.com>
2021-08-13 00:15:01 +0200

    Build of version 0.6.0

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-docker build . -t tracardi/tracardi-api:0.6.0.rc-1
-docker push tracardi/tracardi-api:0.6.0.rc-1
+docker build . -t tracardi/tracardi-api:0.6.0
+docker push tracardi/tracardi-api:0.6.0
 
 #docker build . -t tracardi/tracardi-api
 #docker push tracardi/tracardi-api
