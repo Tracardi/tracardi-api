34959fe4421a3d5ac857087e68ad39aceb232aa5
risto <risto.kowaczewski@gmail.com>
2021-09-07 01:41:02 +0200

    refactoring. Storage decoupling.

--- app/api/generic_endpoint.py
+++ app/api/generic_endpoint.py
@@ -2,11 +2,12 @@ from typing import Optional
 
 from fastapi import APIRouter
 from fastapi import HTTPException, Depends
+from tracardi.service.storage.factory import storage
 
 from .auth.authentication import get_current_user
 from tracardi.domain.enum.indexes_histogram import IndexesHistogram
 from tracardi.domain.enum.indexes_search import IndexesSearch
-from tracardi.domain.index import Index
+from tracardi.service.storage.helpers.index import Index
 from tracardi.domain.sql_query import SqlQuery
 from tracardi.domain.time_range_query import DatetimeRangePayload
 
@@ -18,7 +19,7 @@ router = APIRouter(
 @router.post("/{index}/select", tags=["generic", "event", "profile", "resource", "rule", "session", "flow", "segment"])
 async def select_by_sql(index: IndexesSearch, query: Optional[SqlQuery] = None):
     try:
-        elastic_index = Index(index.value)
+        elastic_index = Index(storage(index.value))
         if query is None:
             query = SqlQuery()
         return await elastic_index.search(query.where, query.limit)
@@ -29,7 +30,7 @@ async def select_by_sql(index: IndexesSearch, query: Optional[SqlQuery] = None):
 @router.post("/{index}/select/range", tags=["generic", "event", "profile", "resource", "rule", "session", "flow", "segment"])
 async def time_range_with_sql(index: IndexesHistogram, query: DatetimeRangePayload):
     try:
-        elastic_index = Index(index.value)
+        elastic_index = Index(storage(index.value))
         return await elastic_index.time_range(query)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -38,7 +39,7 @@ async def time_range_with_sql(index: IndexesHistogram, query: DatetimeRangePaylo
 @router.post("/{index}/select/histogram", tags=["generic", "event", "profile", "resource", "rule", "session", "flow", "segment"])
 async def histogram_with_sql(index: IndexesHistogram, query: DatetimeRangePayload):
     try:
-        elastic_index = Index(index.value)
+        elastic_index = Index(storage(index.value))
         return await elastic_index.histogram(query)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
---
+++ app/api/track/service/merging.py
@@ -0,0 +1,10 @@
+from typing import Union
+from tracardi.domain.profile import Profiles
+from tracardi.service.storage.helpers.profile_loader import load_profiles_to_merge
+
+
+def merge(profile, limit=2000) -> Union[Profiles, None]:
+    # Merging, schedule save only if there is an update in flow.
+    if profile.operation.needs_merging() and profile.operation.needs_update():
+        return await profile.merge(load_profiles_to_merge, limit=limit)
+    return None
