5d53702787e2d3cfec46c508dddda643ab1e48d9
DawidekZagajnik <krukdawid27@gmail.com>
2021-11-11 13:30:21 +0100

    exception fix

--- app/api/event_tag_endpoint.py
+++ app/api/event_tag_endpoint.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from app.config import server
 from .auth.authentication import get_current_user
 from tracardi.domain.event_tag import EventTag
+from tracardi.exceptions.exception import StorageException
 
 router = APIRouter(
     dependencies=[Depends(get_current_user)]
@@ -18,21 +19,19 @@ async def add_tags(tag_form: EventTag):
     )
     if result.errors:
         raise HTTPException(status_code=500, detail=result.errors)
-    return {"OK": {"new": result.saved, "updated": 1-result.saved}}
+    return {"new": result.saved, "updated": 1-result.saved}
 
 
 @router.delete("/event/tag/delete", tags=["event"], include_in_schema=server.expose_gui_api, response_model=dict)
 async def delete_tags(tag_form: EventTag):
-    total, removed, result = await storage.driver.tag.remove(
-        event_type=tag_form.type,
-        tags=tag_form.tags
-    )
-    if total == 0:
-        return {"OK": {"removed": removed, "total": total}} if result["result"] == "deleted" else \
-            {"ERROR": {"details": result["result"]}}
-    else:
-        return {"OK": {"removed": removed, "total": total}} if not result.errors else \
-            {"ERROR": {"details": result.errors}}
+    try:
+        total, removed, result = await storage.driver.tag.remove(
+            event_type=tag_form.type,
+            tags=tag_form.tags
+        )
+    except StorageException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return {"removed": removed, "total": total}
 
 
 @router.get("/event/tag/get", tags=["event"], include_in_schema=server.expose_gui_api, response_model=List[dict])
