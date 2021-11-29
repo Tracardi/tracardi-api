3701c7f3774701f1cf33eb21c3a508317a1b0fc3
DawidekZagajnik <krukdawid27@gmail.com>
2021-11-29 09:23:37 +0100

    usecase #196 + prepared for #211 (consent plug-in)

--- app/api/consent_type_endpoint.py
+++ app/api/consent_type_endpoint.py
@@ -2,9 +2,12 @@ from fastapi import APIRouter, Depends, HTTPException
 from app.api.auth.authentication import get_current_user
 from app.config import server
 from pydantic import BaseModel, validator
-from uuid import UUID, uuid4
 from tracardi.service.storage.driver import storage
 from elasticsearch import ElasticsearchException
+from tracardi.domain.consent_schema import ConsentSchema
+from uuid import UUID
+
+# todo refactoring, change name from consent_type_endpoint to consent_endpoint
 
 
 class ConsentTypePayload(BaseModel):
@@ -19,8 +22,6 @@ class ConsentTypePayload(BaseModel):
             raise ValueError("'default_value' must be either 'grant' or 'deny'")
         return v
 
-class ConsentType(ConsentTypePayload):
-    id: UUID = uuid4()
 
 router = APIRouter(
     dependencies=[Depends(get_current_user)]
@@ -30,14 +31,14 @@ router = APIRouter(
 @router.post("/consent", tags=["consent"], include_in_schema=server.expose_gui_api, response_model=dict)
 async def add_consent_type(data: ConsentTypePayload):
     try:
-        result = await storage.driver.consent_type.add_consent(**ConsentType(**data.dict()).dict())
+        result = await storage.driver.consent_type.add_consent(id=data.name.lower().replace(" ", "-"), **data.dict())
     except ElasticsearchException as e:
         raise HTTPException(status_code=500, detail=str(e))
     return result
 
 
 @router.delete("/consent/{consent_id}", tags=["consent"], include_in_schema=server.expose_gui_api, response_model=dict)
-async def delete_consent_type(consent_id: UUID):
+async def delete_consent_type(consent_id: str):
     try:
         result = await storage.driver.consent_type.delete_by_id(consent_id)
     except ElasticsearchException as e:
@@ -52,3 +53,16 @@ async def get_consent_types(start: int = 0, limit: int = 10):
     except ElasticsearchException as e:
         raise HTTPException(status_code=500, detail=str(e))
     return {"total": len(result), "result": list(result)}
+
+
+@router.post("/consents/id/{profile_id}", tags=["consent"], include_in_schema=server.expose_gui_api, response_model=dict)
+async def add_consent(profile_id: UUID, consent: ConsentSchema):
+    try:
+        profile_to_modify = await storage.driver.profile.load_by_id(str(profile_id))
+        if profile_to_modify is None:
+            raise HTTPException(status_code=500, detail="There is no profile with following id: {}".format(profile_id))
+        profile_to_modify.consents[consent.id] = consent.content[consent.id]
+        result = await storage.driver.profile.save_profile(profile_to_modify)
+    except ElasticsearchException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return result
