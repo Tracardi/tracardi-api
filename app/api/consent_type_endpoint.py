40d3ea9b40b79235e24f082d3a423024be08810a
risto <risto.kowaczewski@gmail.com>
2021-11-30 20:48:06 +0100

    Consent type - removed obsolite data

--- app/api/consent_type_endpoint.py
+++ app/api/consent_type_endpoint.py
@@ -2,12 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from app.api.auth.authentication import get_current_user
 from app.config import server
 from pydantic import BaseModel, validator
-
-from tracardi.domain.storage_result import StorageResult
 from tracardi.service.storage.driver import storage
 from elasticsearch import ElasticsearchException
-from tracardi.domain.consent_schema import ConsentSchema
-from uuid import UUID
 
 
 class ConsentTypePayload(BaseModel):
@@ -53,18 +49,3 @@ async def get_consent_types(start: int = 0, limit: int = 10):
     except ElasticsearchException as e:
         raise HTTPException(status_code=500, detail=str(e))
     return {"total": len(result), "result": list(result)}
-
-# todo after second thought this should be removed
-# todo consent should be a part of workflow
-# @router.post("/consent/profile/{profile_id}", tags=["consent"], include_in_schema=server.expose_gui_api, response_model=dict)
-# async def add_consent(profile_id: UUID, consent: ConsentSchema):
-#     try:
-#         profile_to_modify = await storage.driver.profile.load_by_id(str(profile_id))
-#         if profile_to_modify is None:
-#             raise HTTPException(status_code=500, detail="There is no profile with id: {}".format(profile_id))
-#
-#         profile_to_modify.consents[consent.id] = consent.content[consent.id]
-#         result = await storage.driver.profile.save_profile(profile_to_modify)
-#     except ElasticsearchException as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-#     return result
