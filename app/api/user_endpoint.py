35997298ffed2577ce5acf37b1406c8ad27b09e3
DawidekZagajnik <krukdawid27@gmail.com>
2021-12-06 19:11:11 +0100

    user management backend

--- app/api/user_endpoint.py
+++ app/api/user_endpoint.py
@@ -4,31 +4,21 @@ from app.config import server
 from tracardi.service.storage.driver import storage
 from pydantic import BaseModel
 from typing import List, Optional
-from tracardi.domain.user import User
 from uuid import uuid4, UUID
-from hashlib import sha1
 from elasticsearch import ElasticsearchException
 
 
-class UserForm(BaseModel):
-    id: Optional[str] = uuid4()
-    username: str
+class UserPayload(BaseModel):
     password: str
     full_name: str
     email: str
     roles: List[str]
     disabled: bool = False
 
-    def encode(self, salt: str) -> User:
-        return User(
-            id=self.id,
-            username=sha1((salt + self.username).encode()).hexdigest(),
-            password=sha1((salt + self.password).encode()).hexdigest(),
-            full_name=self.full_name,
-            email=self.email,
-            roles=self.roles,
-            disabled=self.disabled
-        )
+
+class NewUserPayload(UserPayload):
+    id: Optional[UUID] = uuid4()
+    username: str
 
 
 router = APIRouter(
@@ -38,13 +28,26 @@ router = APIRouter(
 )
 
 
-@router.post("/user", tags=["user"], include_in_schema=server.expose_gui_api, response_model=dict)
-async def add_user(user: UserForm):
-    try:
-        result = await storage.driver.user.add_user(user)
-    except ElasticsearchException as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    return {"inserted": result["saved"]}
+@router.post("/user/create", tags=["user"], include_in_schema=server.expose_gui_api, response_model=dict)
+async def add_user(user: NewUserPayload):
+    user_exists = await storage.driver.user.check_if_exists(user.username, user.id)
+    if not user_exists:
+        try:
+            result = await storage.driver.user.add_user(
+                id=str(user.id),
+                username=user.username,
+                password=user.password,
+                full_name=user.full_name,
+                email=user.email,
+                roles=user.roles,
+                disabled=user.disabled
+            )
+        except ElasticsearchException as e:
+            raise HTTPException(status_code=500, detail=str(e))
+        return {"inserted": result.saved}
+    else:
+        raise HTTPException(status_code=500, detail=f"User with username '{user.username}' already exists or chosen "
+                                                    f"ID is already taken. Try to refresh site or change username.")
 
 
 @router.delete("/user/{id}", tags=["user"], include_in_schema=server.expose_gui_api, response_model=dict)
@@ -55,5 +58,5 @@ async def delete_user(id: UUID):
             raise HTTPException(status_code=404, detail=f"Profile with id '{id}' not found")
     except ElasticsearchException as e:
         raise HTTPException(status_code=500, detail=str(e))
-    return {"deleted": 1 if "deleted" in result else 0}
+    return {"deleted": 1 if result["result"] == "deleted" else 0}
 
