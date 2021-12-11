2d7beff1a17a9cf9ca1d885223c859002ddc8989
DawidekZagajnik <krukdawid27@gmail.com>
2021-12-11 17:13:41 +0100

    first commit

---
+++ app/api/validation_schema_endpoint.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends, HTTPException
+from app.config import server
+from app.api.auth.authentication import get_current_user
+from tracardi.domain.event_payload_validator import EventPayloadValidator
+from tracardi.service.storage.driver import storage
+from elasticsearch import ElasticsearchException
+from typing import Optional
+
+router = APIRouter(
+    dependencies=[
+        Depends(get_current_user)
+    ]
+)
+
+
+@router.post("/validation-schema", tags=["validation"], include_in_schema=server.expose_gui_api, response_model=dict)
+async def add_schema(schema: EventPayloadValidator):
+    try:
+        result = await storage.driver.validation_schema.add_schema(schema)
+    except ElasticsearchException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return {"added": result.saved}
+
+
+@router.delete("/validation-schema/{event_type}", tags=["validation"], include_in_schema=server.expose_gui_api,
+               response_model=dict)
+async def del_schema(event_type: str):
+    try:
+        result = await storage.driver.validation_schema.del_schema(event_type)
+    except ElasticsearchException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return {"deleted": 1 if result is not None and result["result"] == "deleted" else 0}
+
+
+@router.get("/validation-schemas/{start=0}/{limit=10}", tags=["validation"], include_in_schema=server.expose_gui_api,
+            response_model=list)
+async def list_schemas(start: Optional[int] = 0, limit: Optional[int] = 10):
+    try:
+        result = await storage.driver.validation_schema.load_schemas(start, limit)
+    except ElasticsearchException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return list(result)
--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ from app.api import token_endpoint, rule_endpoint, resource_endpoint, event_endp
     tql_endpoint, health_endpoint, session_endpoint, instance_endpoint, plugins_endpoint, test_endpoint, \
     settings_endpoint, \
     purchases_endpoint, event_tag_endpoint, consent_type_endpoint, flow_action_endpoint, flows_endpoint, info_endpoint,\
-    user_endpoint
+    user_endpoint, validation_schema_endpoint
 from app.api.auth.authentication import get_current_user
 from app.api.graphql.profile import graphql_profiles
 from app.api.scheduler import tasks_endpoint
@@ -146,6 +146,7 @@ application.include_router(event_tag_endpoint.router)
 application.include_router(consent_type_endpoint.router)
 application.include_router(info_endpoint.router)
 application.include_router(user_endpoint.router)
+application.include_router(validation_schema_endpoint.router)
 
 # GraphQL
 
