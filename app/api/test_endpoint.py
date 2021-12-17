47527e366b4cf0ca1a8a53af79ff338ad7dbd1b4
risto <risto.kowaczewski@gmail.com>
2021-12-17 13:38:10 +0100

    Event source test

--- app/api/test_endpoint.py
+++ app/api/test_endpoint.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 
 from app.config import server
-from tracardi.domain.resource import Resource
+from tracardi.domain.event_source import EventSource
 from tracardi.service.storage.driver import storage
 
 router = APIRouter()
@@ -9,12 +9,11 @@ router = APIRouter()
 
 @router.get("/test/resource", tags=["test"], include_in_schema=server.expose_gui_api)
 async def create_test_data():
-    resource = Resource(
+    resource = EventSource(
         id="@test-resource",
         type="web-page",
         name="Test resource",
         description="This resource is created for test purposes.",
-        consent=True
     )
-    return await storage.driver.resource.save_record(resource)
+    return await storage.driver.event_source.save(resource)
 
