7fd6821bedc8e91abe5de56ded0b14df466a5946
DawidekZagajnik <60825727+DawidekZagajnik@users.noreply.github.com>
2021-12-18 21:28:13 +0100

    validation index mapping, new validation schema endpoint (#200) (#28)
    * Changed index and models so we don't use to_exclude
    * Merge branch 'master' of https://github.com/Tracardi/tracardi-api into Tracardi-master
    
    # Conflicts:
    #	app/main.py
    
    * did testing, all passed
    Co-authored-by: atompie <risto.kowaczewski@gmail.com>

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ from app.api import token_endpoint, rule_endpoint, resource_endpoint, event_endp
     tql_endpoint, health_endpoint, session_endpoint, instance_endpoint, plugins_endpoint, test_endpoint, \
     settings_endpoint, event_source_endpoint, \
     purchases_endpoint, event_tag_endpoint, consent_type_endpoint, flow_action_endpoint, flows_endpoint, info_endpoint,\
-    user_endpoint, pro_endpoint
+    user_endpoint, pro_endpoint, validation_schema_endpoint
 from app.api.auth.authentication import get_current_user
 from app.api.graphql.profile import graphql_profiles
 from app.api.scheduler import tasks_endpoint
@@ -148,6 +148,7 @@ application.include_router(info_endpoint.router)
 application.include_router(user_endpoint.router)
 application.include_router(event_source_endpoint.router)
 application.include_router(pro_endpoint.router)
+application.include_router(validation_schema_endpoint.router)
 
 # GraphQL
 
