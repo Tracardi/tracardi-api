b361ea173b9c086d2a5aadb46045d8ee61759f5a
risto <risto.kowaczewski@gmail.com>
2022-01-09 01:32:47 +0100

    Google Sheets plugin

--- app/api/resource_endpoint.py
+++ app/api/resource_endpoint.py
@@ -200,6 +200,22 @@ async def get_resource_types(type: TypeEnum) -> dict:
                 },
                 "tags": ['token'],
                 "name": "Token"
+            },
+            "google-cloud-service-account": {
+                "config": {
+                    "type": "service_account",
+                    "project_id": "<project-id>",
+                    "private_key_id": "<private-key-id>",
+                    "private_key": "<private-key>",
+                    "client_email": "<client-email>",
+                    "client_id": "<client-id>",
+                    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                    "token_uri": "https://oauth2.googleapis.com/token",
+                    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                    "client_x509_cert_url": "<client-x509-cert-url>"
+                },
+                "tags": ['gcp-service-account'],
+                "name": "Google Cloud Service Account"
             }
         })
 
--- manual/en/docs/flow/actions/send_to_slack_channel_action.md
+++ manual/en/docs/flow/actions/send_to_slack_channel_action.md
@@ -4,6 +4,19 @@ This plugin posts a text message to defined channel on Slack.
 
 ## Requirements
 This plugin requires adding an app to your Slack workspace:
+ 
+- Go to https://api.slack.com/apps - open account if you do not have one.
+- Create new app
+- Select from scratch option
+- Type a name for the app and pick a workspace
+- Click "Incoming Webhooks"
+- Turn it on (upper right conner)
+- Add new webhook to workspace
+- Select a channel and click Allow
+- On the list at the bottom of page select your webhook and click copy
+- You have copied the webhook with token 
+- Paste the token when creating Tracardi resource
+
 - Add app to your workspace
 - Allow this app to write into channels
 - Copy this app's bot token
