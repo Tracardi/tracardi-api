c19318de9908c0ce19b1d5ca61a6230d514bdf76
DawidekZagajnik <krukdawid27@gmail.com>
2022-02-07 20:17:01 +0100

    mautic segmentation plugin, draft version

--- app/setup/on_start.py
+++ app/setup/on_start.py
@@ -167,6 +167,7 @@ async def add_plugins():
         'tracardi.process_engine.action.v1.connectors.mautic.fetch_contact_by_email.plugin',
         'tracardi.process_engine.action.v1.connectors.mautic.add_points.plugin',
         'tracardi.process_engine.action.v1.connectors.mautic.subtract_points.plugin',
+        'tracardi.process_engine.action.v1.connectors.mautic.change_segment.plugin',
 
         # Internal
         'tracardi.process_engine.action.v1.internal.event_source_fetcher.plugin',
