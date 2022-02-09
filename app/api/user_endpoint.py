eb7919c670773891187c52fb95b64ace436dec60
DawidekZagajnik <krukdawid27@gmail.com>
2022-02-09 23:45:36 +0100

    user site

--- app/api/user_endpoint.py
+++ app/api/user_endpoint.py
@@ -4,7 +4,7 @@ from app.config import server
 from tracardi.service.storage.driver import storage
 from pydantic import BaseModel
 from typing import List, Optional
-from uuid import uuid4, UUID
+from uuid import UUID
 from elasticsearch import ElasticsearchException
 
 
@@ -17,7 +17,7 @@ class UserPayload(BaseModel):
 
 
 class NewUserPayload(UserPayload):
-    id: Optional[UUID] = uuid4()
+    id: UUID
     username: str
 
 
@@ -26,6 +26,16 @@ router = APIRouter(
         Depends(get_current_user)
     ]
 )
+
+
+@router.get("/user/refresh", tags=["user"], include_in_schema=server.expose_gui_api, response_model=dict)
+async def refresh_users():
+    return await storage.driver.user.refresh()
+
+
+@router.get("/user/flush", tags=["user"], include_in_schema=server.expose_gui_api, response_model=dict)
+async def flush_users():
+    return await storage.driver.user.flush()
 
 
 @router.post("/user", tags=["user"], include_in_schema=server.expose_gui_api, response_model=dict)
@@ -62,3 +72,49 @@ async def delete_user(id: UUID):
         raise HTTPException(status_code=500, detail=str(e))
     return {"deleted": 1 if result["result"] == "deleted" else 0}
 
+
+@router.get("/user/{id}", tags=["user"], include_in_schema=server.expose_gui_api, response_model=dict)
+async def get_user(id: UUID):
+    try:
+        result = await storage.driver.user.get_by_id(id)
+        if result is None:
+            raise HTTPException(status_code=404, detail=f"User with ID {id} not found.")
+    except ElasticsearchException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return UserPayload(**result)
+
+
+@router.get("/users/{start}/{limit}", tags=["user"], include_in_schema=server.expose_gui_api, response_model=list)
+async def get_users(start: int = 0, limit: int = 100, query: Optional[str] = ""):
+    try:
+        result = await storage.driver.user.search_by_name(start, limit, query) if query else \
+            await storage.driver.user.load(start, limit)
+    except ElasticsearchException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return list(map(lambda record: record["_source"] if query else record, result["hits"]["hits"] if
+                query else list(result)))
+
+
+@router.post("/users/{id}/edit", tags=["user"], include_in_schema=server.expose_gui_api, response_model=dict)
+async def edit_user(id: UUID, user: UserPayload):
+    try:
+        current_user = await storage.driver.user.get_by_id(id)
+    except ElasticsearchException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    if current_user:
+        try:
+            result = await storage.driver.user.edit_user(
+                id=str(id),
+                username=current_user["username"],
+                password=user.password if user.password != current_user["password"] else current_user["password"],
+                full_name=user.full_name,
+                email=user.email,
+                roles=user.roles,
+                disabled=user.disabled,
+                password_change=user.password != current_user["password"]
+            )
+        except ElasticsearchException as e:
+            raise HTTPException(status_code=500, detail=str(e))
+        return {"inserted": result.saved}
+    else:
+        raise HTTPException(status_code=404, detail=f"User with ID '{id}' does not exist")
