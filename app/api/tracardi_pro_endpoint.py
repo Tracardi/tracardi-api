b6fe80891c7d5613b4ff98ec2902779ef8a14c9e
risto <risto.kowaczewski@gmail.com>
2022-02-12 00:28:19 +0100

    Tracardi Pro

--- app/api/tracardi_pro_endpoint.py
+++ app/api/tracardi_pro_endpoint.py
@@ -3,9 +3,11 @@ import logging
 import grpc
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import ValidationError
+from tracardi.service.storage.factory import StorageFor
 
 from app.api.domain.credentials import Credentials
 from tracardi.config import tracardi
+from tracardi.domain.resource import Resource, ResourceRecord
 from tracardi.domain.sign_up_data import SignUpData, SignUpRecord
 from app.api.proto.tracard_pro_client import TracardiProClient
 from tracardi.exceptions.log_handler import log_handler
@@ -89,14 +91,15 @@ async def tracardi_pro_sign_up(sign_up_data: SignUpData):
         return True
 
     except grpc.RpcError as e:
-        raise HTTPException(detail="Access denied due to RPC error \"{}\". Error status: {}".format(e.details(), e.code().name),
-                            status_code=403)  # Must be 403 because 401 logs out gui
+        raise HTTPException(
+            detail="Access denied due to RPC error \"{}\". Error status: {}".format(e.details(), e.code().name),
+            status_code=403)  # Must be 403 because 401 logs out gui
     except Exception as e:
         raise HTTPException(status_code=500, detail=repr(e))
 
 
 @router.get("/tpro/available_services", tags=["tpro"], include_in_schema=server.expose_gui_api)
-async def get_tracardi_pro_endpoint():
+async def get_available_services():
     try:
         return tracardi_pro_client.get_available_services()
 
@@ -106,10 +109,39 @@ async def get_tracardi_pro_endpoint():
 
 
 @router.get("/tpro/available_hosts", tags=["tpro"], include_in_schema=server.expose_gui_api)
-async def get_tracardi_pro_endpoint():
+async def get_available_hosts():
     try:
         return tracardi_pro_client.get_available_hosts()
 
     except grpc.RpcError as e:
         # Must be 403 because 401 logs out gui
         raise HTTPException(detail=e.details(), status_code=403 if e.code().name == 'UNAUTHENTICATED' else 500)
+
+
+@router.post("/tpro/resource", tags=["tpro"], include_in_schema=server.expose_gui_api)
+async def save_tracardi_pro_resource(resource: Resource):
+
+    try:
+        sign_up_record = await storage.driver.pro.read_pro_service_endpoint()
+    except ValidationError as e:
+        raise HTTPException(detail=f"Validation error when reading pro service user data: {str(e)}", status_code=500)
+
+    def _remove_redundant_data(credentials):
+        credentials.pop('name', None)
+        credentials.pop('description', None)
+        return credentials
+
+    resource.credentials.production = _remove_redundant_data(resource.credentials.production)
+    resource.credentials.test = _remove_redundant_data(resource.credentials.test)
+
+    resource.credentials.test['host'] = sign_up_record.host
+    resource.credentials.test['token'] = sign_up_record.token
+
+    resource.credentials.production['host'] = sign_up_record.host
+    resource.credentials.production['token'] = sign_up_record.token
+
+    try:
+        record = ResourceRecord.encode(resource)
+        return await StorageFor(record).index().save()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
