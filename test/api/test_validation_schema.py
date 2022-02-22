802344b000a46e57e545adf4df14dd13fdc6be32
risto <risto.kowaczewski@gmail.com>
2022-02-22 12:49:03 +0100

    tests fixes.

--- test/api/test_validation_schema.py
+++ test/api/test_validation_schema.py
@@ -5,10 +5,11 @@ endpoint = Endpoint()
 
 
 def test_put_validation_schema_refresh():
+    # todo remove this test makes no sense you cant test refresh this way
     endpoint.put("/event/validation-schema/refresh")
 
 
-def test_post_validation_schema():
+def test_should_post_validation_schema():
     data = {
         "event_type": "test-type",
         "name": "test-name",
@@ -30,7 +31,7 @@ def test_post_validation_schema():
     endpoint.delete("/event/validation-schema/test-type")
 
 
-def test_get_validation_schema_by_type():
+def test_should_get_validation_schema_by_type():
     try:
         result = endpoint.get("/event/validation-schema/test-type")
         result = result.json()
@@ -40,36 +41,33 @@ def test_get_validation_schema_by_type():
         assert e.status_code == 404
 
 
-def test_delete_validation_schema_by_type():
-    data = {
-        "event_type": "test-type",
-        "name": "test-name",
-        "description": "test-description",
-        "enabled": True,
-        "tags": ["tag1", "tag2", "tag3"],
-        "validation": {
-            "event@...": {
-                "type": "object"
+def test_should_delete_validation_schema_by_type():
+    try:
+        data = {
+            "event_type": "test-type",
+            "name": "test-name",
+            "description": "test-description",
+            "enabled": True,
+            "tags": ["tag1", "tag2", "tag3"],
+            "validation": {
+                "event@...": {
+                    "type": "object"
+                }
             }
         }
-    }
 
-    result = endpoint.post("/event/validation-schema", data)
-    result = result.json()
+        result = endpoint.post("/event/validation-schema", data)
+        result = result.json()
 
-    print(result)
+        assert "added" in result
 
-    assert "added" in result
+        result = endpoint.delete("/event/validation-schema/test-type")
+        result = result.json()
 
-    result = endpoint.delete("/event/validation-schema/test-type")
-    result = result.json()
+        assert result["deleted"] == 1
 
-    assert result["deleted"] == 1
-
-    result = endpoint.delete("/event/validation-schema/test-type")
-    result = result.json()
-
-    assert result["deleted"] == 0
+    finally:
+        endpoint.delete("/event/validation-schema/test-type")
 
 
 def test_get_validation_schemas():
