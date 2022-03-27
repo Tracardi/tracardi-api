c4fbb41860532a3c1401d516a9dc26218dbf8338
DawidekZagajnik <krukdawid27@gmail.com>
2022-03-27 19:44:43 +0200

    user self-edit site

--- app/api/user_endpoint.py
+++ app/api/user_endpoint.py
@@ -25,6 +25,11 @@ class UserPayload(BaseModel):
         return value
 
 
+class UserSoftEditPayload(BaseModel):
+    password: str
+    full_name: str
+
+
 router = APIRouter(
     dependencies=[Depends(Permissions(roles=["admin"]))]
 )
@@ -128,7 +133,7 @@ async def update_user(id: str, user_payload: UserPayload):
 
     if current_user:
         try:
-            user = User(**user_payload.dict(), id=user_payload.email)
+            user = User(**user_payload.dict(), id=user_payload.email, token=current_user["token"])
 
             if user_payload.password != current_user["password"]:
                 user.encode_password()
@@ -141,3 +146,21 @@ async def update_user(id: str, user_payload: UserPayload):
         return {"inserted": result.saved}
     else:
         raise HTTPException(status_code=404, detail=f"User '{id}' does not exist")
+
+
+@router.get("/user-account", tags=["user"], include_in_schema=server.expose_gui_api, response_model=dict)
+async def get_self(user=Depends(Permissions(["admin", "developer", "marketer"]))):
+    """
+    Returns data of the user who called the endpoint
+    """
+    return user.dict()
+
+
+@router.post("/user-account", tags=["user"], include_in_schema=server.expose_gui_api, response_model=dict)
+async def soft_edit(payload: UserSoftEditPayload, user=Depends(Permissions(["admin", "developer", "marketer"]))):
+    try:
+        result = await update_user(user.id, UserPayload(**payload.dict(), roles=user.roles, disabled=user.disabled,
+                                                        email=user.email))
+        return result
+    except ElasticsearchException as e:
+        raise HTTPException(status_code=500, detail=str(e))
