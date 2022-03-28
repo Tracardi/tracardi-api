5f34f5869349cd2c861db3c573a131fe14db9f4e
risto <risto.kowaczewski@gmail.com>
2022-03-28 21:33:01 +0200

    Refactoring of user acccount edit.

--- app/api/user_account_endpoint.py
+++ app/api/user_account_endpoint.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
 from app.config import server
 from pydantic import ValidationError
-from app.api.user_endpoint import UserPayload, UserSoftEditPayload, update_user
+from app.api.user_endpoint import UserPayload, UserSoftEditPayload
 
 from elasticsearch import ElasticsearchException
 
 from .auth.permissions import Permissions
-
+from ..service.user_manager import update_user
 
 router = APIRouter(
     dependencies=[Depends(Permissions(roles=["admin", "marketer", "developer"]))]
@@ -22,11 +22,18 @@ async def get_self(user=Depends(Permissions(["admin", "developer", "marketer"]))
 
 
 @router.post("/user-account", tags=["user"], include_in_schema=server.expose_gui_api, response_model=dict)
-async def soft_edit(payload: UserSoftEditPayload, user=Depends(Permissions(["admin", "developer", "marketer"]))):
+async def edit_user_account(payload: UserSoftEditPayload,
+                            user=Depends(Permissions(["admin", "developer", "marketer"]))):
     try:
-        result = await update_user(user.id, UserPayload(**payload.dict(), roles=user.roles, disabled=user.disabled,
-                                                        email=user.email), user)
-        return result
+        saved = await update_user(user.id,
+                                  UserPayload(**payload.dict(),
+                                              roles=user.roles,
+                                              disabled=user.disabled,
+                                              email=user.email)
+                                  )
+        return {"inserted": saved}
+    except LookupError as e:
+        raise HTTPException(status_code=404, detail=str(e))
     except ValidationError as e:
         raise HTTPException(status_code=403, detail=str(e.raw_errors[0].exc))
     except ElasticsearchException as e:
