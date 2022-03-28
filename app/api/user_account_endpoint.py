cf33ed1d2fb828082d13d9dd472b9fc6285d6363
risto <risto.kowaczewski@gmail.com>
2022-03-28 22:37:18 +0200

    Conditional destination sync.

--- app/api/user_account_endpoint.py
+++ app/api/user_account_endpoint.py
@@ -14,7 +14,7 @@ router = APIRouter(
 
 
 @router.get("/user-account", tags=["user"], include_in_schema=server.expose_gui_api, response_model=dict)
-async def get_self(user=Depends(Permissions(["admin", "developer", "marketer"]))):
+async def get_user_account(user=Depends(Permissions(["admin", "developer", "marketer"]))):
     """
     Returns data of the user who called the endpoint
     """
