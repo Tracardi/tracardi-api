2f4980c35a4df8705efbebb3c0ba26c9a8dfbb8c
DawidekZagajnik <60825727+DawidekZagajnik@users.noreply.github.com>
2022-04-13 22:40:46 +0200

    Redis and ES health check endpoints (#74)

--- app/api/test_endpoint.py
+++ app/api/test_endpoint.py
@@ -1,11 +1,15 @@
 import asyncio
 
 from fastapi import APIRouter, Depends
+from tracardi.service.storage.redis_client import RedisClient
+from tracardi.service.storage.elastic_client import ElasticClient
+
 from app.api.auth.permissions import Permissions
 from app.config import server
 from app.service.data_generator import generate_fake_data, generate_random_date
 from tracardi.domain.event_source import EventSource
 from tracardi.service.storage.driver import storage
+from fastapi import HTTPException
 
 router = APIRouter(
     dependencies=[Depends(Permissions(roles=["admin"]))]
@@ -39,3 +43,30 @@ async def make_fake_data():
                 record['metadata']['time']['insert'] = generate_random_date()
             await storage.driver.raw.index(index).upsert(record)
 
+
+@router.get("/test/redis", tags=["test"], include_in_schema=server.expose_gui_api)
+async def ping_redis():
+    """
+    Tests connection between Redis instance and Tracardi instance. Accessible for roles: "admin"
+    """
+    client = RedisClient()
+    try:
+        pong = client.client.ping()
+        if pong is not True:
+            raise HTTPException(status_code=500, detail="Redis did not respond.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/test/elasticsearch", tags=["test"], include_in_schema=server.expose_gui_api)
+async def get_indices():
+    """
+    Tests connection between Elasticsearch and Tracardi by listing indices. Accessible for roles: "admin"
+    """
+    es = ElasticClient.instance()
+    try:
+        health = await es.cluster.health()
+        if not isinstance(health, dict):
+            raise HTTPException(status_code=500, detail="Elasticsearch did not pass health check.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
