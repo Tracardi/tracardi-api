0f88e470425e435181a9acc8dc4f42de8588849b
risto <risto.kowaczewski@gmail.com>
2022-04-21 17:47:56 +0200

    TPRO schduler plugin.

--- app/api/proto/tracard_pro_client.py
+++ app/api/proto/tracard_pro_client.py
@@ -8,7 +8,7 @@ from google.protobuf import json_format
 
 from tracardi.config import tracardi
 from tracardi.exceptions.log_handler import log_handler
-from tracardi.service.storage.driver import storage
+from tracardi.service.pro.auth import get_tpro_token
 
 _local_path = os.path.dirname(__file__)
 logging.basicConfig(level=logging.ERROR)
@@ -39,22 +39,6 @@ class TracardiProClient(object):
         # bind the client and the server
         self.stub = pb2_grpc.ServiceStub(self.channel)
 
-    async def _get_token(self):
-        """
-        Return None if not configured otherwise returns token.
-        """
-        try:
-            # todo add cache
-            result = await storage.driver.pro.read_pro_service_endpoint()
-        except Exception as e:
-            logger.error(f"Exception when reading pro service user data: {str(e)}")
-            result = None
-
-        if result is None:
-            return None
-
-        return result.token
-
     def validate(self, token) -> Optional[str]:
         try:
             message = pb2.EmptyParams()
@@ -72,12 +56,12 @@ class TracardiProClient(object):
 
     async def get_available_services(self):
         message = pb2.EmptyParams()
-        services = self.stub.get_available_services(message, metadata=[('token', await self._get_token())])
+        services = self.stub.get_available_services(message, metadata=[('token', await get_tpro_token())])
         return json_format.MessageToDict(services)
 
     async def get_plugin(self, module):
         message = pb2.PluginMetaData(module=module)
-        services = self.stub.get_plugin(message, metadata=[('token', await self._get_token())])
+        services = self.stub.get_plugin(message, metadata=[('token', await get_tpro_token())])
         return json_format.MessageToDict(services)
 
     def sign_up(self, username, password):
