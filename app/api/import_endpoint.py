7a68f1eb2e8fc6e512a3ea167291ede9f0634848
risto <risto.kowaczewski@gmail.com>
2022-05-11 00:34:54 +0200

    import worker

--- app/api/import_endpoint.py
+++ app/api/import_endpoint.py
@@ -1,16 +1,15 @@
 from fastapi import APIRouter, HTTPException, Depends
 
 from tracardi.service.storage.driver import storage
+from worker.celery_worker import celery
 from .auth.permissions import Permissions
 from ..config import server
 from tracardi.exceptions.exception import StorageException
 from tracardi.domain.import_config import ImportConfig
-from tracardi.service.batches import get_batches
-from tracardi.service.module_loader import import_package, is_coroutine
+from tracardi.service.import_types import get_import_types
+from tracardi.service.module_loader import import_package
 from pydantic import ValidationError
-from tracardi.service.storage.redis_client import RedisClient
-from uuid import uuid4
-import asyncio
+from celery.result import AsyncResult
 from starlette.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from app.service.error_converter import convert_errors
@@ -20,19 +19,30 @@ router = APIRouter(
 )
 
 
+@router.get("/import/status/{task_id}", tags=["import"], include_in_schema=server.expose_gui_api)
+def get_status(task_id):
+    task_result = AsyncResult(task_id, app=celery)
+    result = {
+        "task": task_result.id,
+        "status": task_result.status,
+        "result": task_result.result
+    }
+    return result
+
+
 @router.get("/import/types", tags=["import"], include_in_schema=server.expose_gui_api)
-async def get_batch_types():
-    return get_batches()
+async def load_import_types():
+    return get_import_types()
 
 
-@router.get("/import/{id}", tags=["import"], include_in_schema=server.expose_gui_api)
-async def get_batch_by_id(id: str):
+@router.get("/import/{import_id}", tags=["import"], include_in_schema=server.expose_gui_api)
+async def get_import_by_id(import_id: str):
     try:
-        result = await storage.driver.import_config.load(id)
+        result = await storage.driver.import_config.load(import_id)
         if result is not None:
             return result
         else:
-            raise HTTPException(status_code=404, detail=f"No batch found for id {id}")
+            raise HTTPException(status_code=404, detail=f"No import configuration found for id {import_id}")
 
     except StorageException as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -63,10 +73,10 @@ async def add_import_config(import_configuration: dict):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.delete("/import/{id}", tags=["import"], include_in_schema=server.expose_gui_api)
-async def delete_batch(id: str):
+@router.delete("/import/{import_id}", tags=["import"], include_in_schema=server.expose_gui_api)
+async def delete_import_configuration(import_id: str):
     try:
-        result = await storage.driver.import_config.delete(id)
+        result = await storage.driver.import_config.delete(import_id)
         await storage.driver.import_config.refresh()
         return result
 
@@ -75,7 +85,7 @@ async def delete_batch(id: str):
 
 
 @router.get("/imports", tags=["import"], include_in_schema=server.expose_gui_api)
-async def load_batches(limit: int = 100, query: str = None):
+async def load_all_imports(limit: int = 100, query: str = None):
     try:
         result = await storage.driver.import_config.load_all(limit, query)
         return {"grouped": {"General": result}} if result else {}
@@ -84,13 +94,13 @@ async def load_batches(limit: int = 100, query: str = None):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.post("/import/run/{id}", tags=["import"], include_in_schema=server.expose_gui_api)
-async def run_import(id: str, debug: bool = True):
+@router.post("/import/run/{import_id}", tags=["import"], include_in_schema=server.expose_gui_api)
+async def run_import(import_id: str, debug: bool = True):
     try:
 
-        import_configuration = await storage.driver.import_config.load(id)
+        import_configuration = await storage.driver.import_config.load(import_id)
         if import_configuration is None:
-            raise HTTPException(status_code=404, detail=f"No import source configuration found for id {id}")
+            raise HTTPException(status_code=404, detail=f"No import source configuration found for id {import_id}")
 
         if import_configuration.enabled is False:
             raise HTTPException(status_code=409, detail=f"Selected import source is disabled")
@@ -117,35 +127,15 @@ async def run_import(id: str, debug: bool = True):
 
 
 @router.get("/import/form/{module}", tags=["import"], include_in_schema=server.expose_gui_api)
-async def get_batch_form(module: str):
+async def get_import_configuration_form(module: str):
     try:
         module = module.split(".")
         package = import_package(".".join(module[:-1]))
-        batch_class = getattr(package, module[-1])
+        import_configuration = getattr(package, module[-1])
         return {
-            "form": batch_class.form,
-            "init": batch_class.init
+            "form": import_configuration.form,
+            "init": import_configuration.init
         }
 
     except AttributeError as e:
         raise HTTPException(status_code=404, detail=str(e))
-
-
-@router.post("/import/validate-config/{module}", tags=["import"], include_in_schema=server.expose_gui_api)
-async def validate_batch_config(module: str, config: dict):
-    try:
-        if module:
-            module = module.split(".")
-            package = import_package(".".join(module[:-1]))
-            import_class = getattr(package, module[-1])
-            import_class.config_model(**config)
-        else:
-            raise HTTPException(status_code=422, detail="No batch type selected")
-
-    except AttributeError as e:
-        raise HTTPException(status_code=404, detail=str(e))
-    except ValidationError as e:
-        return JSONResponse(
-            status_code=422,
-            content=jsonable_encoder(convert_errors(e))
-        )
--- build-http.sh
+++ build-http.sh
@@ -1,3 +1,3 @@
 git rev-parse HEAD > app/tracker/revision.txt
 docker build . --no-cache -t tracardi/tracardi-api
-docker push tracardi/tracardi-api
+#docker push tracardi/tracardi-api
