4bc73ed268822592fb3722ec2e633545a1efba52
DawidekZagajnik <krukdawid27@gmail.com>
2022-06-04 23:03:10 +0200

    quick fix v2

--- app/api/delete_indices_endpoint.py
+++ app/api/delete_indices_endpoint.py
@@ -4,14 +4,19 @@ from tracardi.config import NAME
 from tracardi.service.storage.elastic_client import ElasticClient
 from elasticsearch import ElasticsearchException
 from .auth.permissions import Permissions
+from typing import Optional
+from hashlib import md5
 
 router = APIRouter(
     dependencies=[Depends(Permissions(roles=["admin"]))]
 )
 
 
-@router.delete("/indices/version/{codename}", tags=["index"], include_in_schema=server.expose_gui_api)
-async def delete_old_indices(codename: str):
+@router.delete("/indices/version/{version}", tags=["index"], include_in_schema=server.expose_gui_api)
+async def delete_old_indices(version: str, codename: Optional[str] = None):
+
+    if codename is None:
+        codename = md5(version.encode('utf-8')).hexdigest()[:5]
 
     if codename == NAME:
         raise HTTPException(status_code=409, detail="Cannot delete currently connected indices.")
@@ -19,7 +24,7 @@ async def delete_old_indices(codename: str):
     try:
         es = ElasticClient.instance()
         indices = await es.list_aliases()
-        to_delete = [alias for alias in indices if f".{codename}.tracardi-" in alias]
+        to_delete = [index for index in indices if index.startswith(f"{version.replace('.', '')}.{codename}.tracardi-")]
 
         result = {}
         for alias in to_delete:
