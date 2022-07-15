9879b55db08c8083114340d65c8e7e3f6a1465a9
risto <risto.kowaczewski@gmail.com>
2022-07-15 10:20:09 +0200

    Version upgrades

--- app/api/info_endpoint.py
+++ app/api/info_endpoint.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter
+from elasticsearch import ElasticsearchException
+from fastapi import APIRouter, HTTPException
 from app.config import server
 from tracardi.config import tracardi
+from tracardi.domain.version import Version
+from tracardi.exceptions.exception import StorageException
+from tracardi.service.storage.driver import storage
 
 router = APIRouter()
 
@@ -11,4 +15,20 @@ async def get_version():
     Returns info about Tracardi API version
     """
     return tracardi.version.version
+
+
+@router.get("/info/version/details", tags=["version"], include_in_schema=server.expose_gui_api, response_model=Version)
+async def get_current_backend_version():
+
+    """
+    Returns current backend version with previous versions.
+    """
+
+    try:
+
+        result = await storage.driver.version.load()
+        return Version(**result)
+
+    except (ElasticsearchException, StorageException) as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
