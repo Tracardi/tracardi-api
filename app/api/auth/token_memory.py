508bd84321ffa8005d6c400d64548e6b7a198eb0
DawidekZagajnik <krukdawid27@gmail.com>
2022-07-15 19:07:36 +0200

    added elastic identifier to redis auth tokens

--- app/api/auth/token_memory.py
+++ app/api/auth/token_memory.py
@@ -1,4 +1,6 @@
 from tracardi.service.storage.redis_client import RedisClient
+from tracardi.config import elastic
+from hashlib import sha1
 
 
 class TokenMemory:
@@ -6,15 +8,16 @@ class TokenMemory:
     def __init__(self):
         self._redis = RedisClient()
         self.ttl = 30 * 60
+        self._elastic_identifier = sha1(f"{elastic.host}".encode("utf-8")).hexdigest()
 
     def __setitem__(self, key, value):
-        self._redis.client.set(f"AUTH-TOKEN-{key}", value, ex=self.ttl)
+        self._redis.client.set(f"AUTH-TOKEN-{self._elastic_identifier}-{key}", value, ex=self.ttl)
 
     def __getitem__(self, key):
-        return self._redis.client.get(f"AUTH-TOKEN-{key}")
+        return self._redis.client.get(f"AUTH-TOKEN-{self._elastic_identifier}-{key}")
 
     def __delitem__(self, key):
-        self._redis.client.delete(f"AUTH-TOKEN-{key}")
+        self._redis.client.delete(f"AUTH-TOKEN-{self._elastic_identifier}-{key}")
 
     def refresh(self, key):
-        self._redis.client.expire(f"AUTH-TOKEN-{key}", self.ttl)
+        self._redis.client.expire(f"AUTH-TOKEN-{self._elastic_identifier}-{key}", self.ttl)
