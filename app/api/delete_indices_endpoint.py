0c74a6cf7ffb6c88f04b1fa11f4562673a685c8e
DawidekZagajnik <krukdawid27@gmail.com>
2022-07-16 23:02:30 +0200

    fixed elastic index removing to use Version

--- app/api/delete_indices_endpoint.py
+++ app/api/delete_indices_endpoint.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
 from app.config import server
-from tracardi.config import NAME
+from tracardi.config import tracardi
 from tracardi.service.storage.elastic_client import ElasticClient
 from elasticsearch import ElasticsearchException
 from .auth.permissions import Permissions
 from typing import Optional
-from hashlib import md5
+from tracardi.domain.version import Version
 
 router = APIRouter(
     dependencies=[Depends(Permissions(roles=["admin"]))]
@@ -15,16 +15,17 @@ router = APIRouter(
 @router.delete("/indices/version/{version}", tags=["index"], include_in_schema=server.expose_gui_api)
 async def delete_old_indices(version: str, codename: Optional[str] = None):
 
-    if codename is None:
-        codename = md5(version.encode('utf-8')).hexdigest()[:5]
+    version = Version(version=version, name=codename)
 
-    if codename == NAME:
-        raise HTTPException(status_code=409, detail="Cannot delete currently connected indices.")
+    if version.has_same_current_version(tracardi.version):
+        raise HTTPException(status_code=409, detail="You cannot delete indices that are currently used.")
 
     try:
         es = ElasticClient.instance()
-        indices = await es.list_aliases()
-        to_delete = [index for index in indices if index.startswith(f"{version.replace('.', '')}.{codename}.tracardi-")]
+        indices = await es.list_indices()
+        to_delete = [index for index in indices if index.startswith(
+            f"{version.get_version_prefix()}.{version.name}.tracardi-"
+        )]
 
         result = {}
         for alias in to_delete:
