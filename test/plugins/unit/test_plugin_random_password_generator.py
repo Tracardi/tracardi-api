f27cf2ed2dafb7866e2f81462f13bb95c8677442
Matt <mefpef@gmail.com>
2022-07-28 20:09:54 +0200

    added documentation for password_generator_action

---
+++ test/plugins/unit/test_plugin_random_password_generator.py
@@ -0,0 +1,17 @@
+from tracardi.process_engine.action.v1.password_generator_action import PasswordGeneratorAction
+from tracardi.service.plugin.service.plugin_runner import run_plugin
+
+
+def test_should_return_password_length():
+    init = {"max_length": 12,
+            "min_length": 10,
+            "uppercase": 5,
+            "lowercase": 5,
+            "special_characters": 2}
+
+    payload = {}
+
+    result = run_plugin(PasswordGeneratorAction, init, payload)
+
+    assert result.output.port == "password"
+    assert (len(result.output.value["password"]) - 1) == init["max_length"]
