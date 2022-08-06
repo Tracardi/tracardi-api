6905195ffea24ee8cfb88d652935e7a6d037e230
DawidekZagajnik <krukdawid27@gmail.com>
2022-08-06 17:24:36 +0200

    modified test

--- test/tracardi/test_report.py
+++ test/tracardi/test_report.py
@@ -5,7 +5,7 @@ from tracardi.domain.report import Report
 def test_report_entities():
     for entity in ("profile", "session", "event", "entity", "improper-value-1", "improper-value-2"):
         try:
-            Report(
+            report = Report(
                 id="@test-report",
                 name="test-report",
                 description="Here's report description.",
@@ -13,6 +13,7 @@ def test_report_entities():
                 query={"query": {"term": {"type": "{{type}}"}}},
                 tags=["tag1", "tag2"]
             )
+            assert report.entity in ("profile", "session", "event", "entity")
 
         except ValidationError as e:
             assert entity in ("improper-value-1", "improper-value-2")
