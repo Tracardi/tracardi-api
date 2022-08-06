f87f42da6e6baf18cec51d89f8ff71fffdc29516
Matt <mefpef@gmail.com>
2022-08-06 10:11:05 +0200

    fix test for random password generator from #607 task

--- test/plugins/unit/test_plugin_random_password_generator.py
+++ test/plugins/unit/test_plugin_random_password_generator.py
@@ -3,7 +3,7 @@ from tracardi.service.plugin.service.plugin_runner import run_plugin
 
 
 def test_should_return_password_length():
-    init = {"max_length": 12,
+    init = {"max_length": 14,
             "min_length": 10,
             "uppercase": 5,
             "lowercase": 5,
@@ -14,4 +14,4 @@ def test_should_return_password_length():
     result = run_plugin(PasswordGeneratorAction, init, payload)
 
     assert result.output.port == "password"
-    assert (len(result.output.value["password"]) - 1) == init["max_length"]
+    assert (len(result.output.value["password"])) == init["max_length"]
