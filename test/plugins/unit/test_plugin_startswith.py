95d9705408bc817728bfff05bcf20136d9d905cf
Mateusz <74150981+Mefpef@users.noreply.github.com>
2022-08-17 20:57:31 +0200

    645 plugin startswith (#133)
    * added documentation for password_generator_action
    * add test for password_generator_action
    * fix test for random password generator from #607 task
    * add documentation for novu plugin
    * add documentation for startswith plugin
    * add tests for startswith plugin

---
+++ test/plugins/unit/test_plugin_startswith.py
@@ -0,0 +1,31 @@
+import pytest
+
+from tracardi.process_engine.action.v1.starts_with_action import StartsWithAction, Config
+from tracardi.service.plugin.service.plugin_runner import run_plugin
+
+
+def test_should_return_true():
+    init = {"field": "payload@name",
+            "prefix": "Test"
+            }
+
+    payload = {"name": "Test"}
+
+    result = run_plugin(StartsWithAction, init, payload)
+    assert result.output.port == "true"
+
+
+def test_should_return_false():
+    init = {"field": "payload@name",
+            "prefix": "test"
+            }
+
+    payload = {"name": "Test"}
+
+    result = run_plugin(StartsWithAction, init, payload)
+    assert result.output.port == "false"
+
+
+def test_empty_prefix_validation():
+    with pytest.raises(ValueError):
+        Config(field="payload@name", prefix="")
