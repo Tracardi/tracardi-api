373261b790eb42a7f37e4868c7a968bde8a9b9c8
risto <risto.kowaczewski@gmail.com>
2022-11-18 19:47:10 +0100

    docker refactoring

--- build-https-as-latest.sh
+++ build-https-as-latest.sh
@@ -1,3 +1,3 @@
 # Uncomment in version branch. DO not use in master branch
-#docker build . --rm --no-cache -f Dockerfile.ssl -t tracardi/tracardi-api-ssl
+#docker build . --rm --no-cache -f docker.ssl.Dockerfile -t tracardi/tracardi-api-ssl
 #docker push tracardi/tracardi-api-ssl
