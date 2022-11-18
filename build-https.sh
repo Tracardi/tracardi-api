2c47947a7d366ca91d17d39de9c08eb1a2daa86e
risto <risto.kowaczewski@gmail.com>
2022-11-18 16:34:51 +0100

    Version 0.7.4-dev

--- build-https.sh
+++ build-https.sh
@@ -1,2 +1,2 @@
-docker build . --rm --no-cache -f Dockerfile.ssl -t tracardi/tracardi-api-ssl:0.7.3-dev
-docker push tracardi/tracardi-api-ssl:0.7.3-dev
+docker build . --rm --no-cache -f Dockerfile.ssl -t tracardi/tracardi-api-ssl:0.7.4-dev
+docker push tracardi/tracardi-api-ssl:0.7.4-dev
