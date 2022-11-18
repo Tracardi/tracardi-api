6b25a8250c4c9f6c1a7e069b5b8cf66aefd7a331
risto <risto.kowaczewski@gmail.com>
2022-11-18 16:30:52 +0100

    Version 0.7.3

--- build-http.sh
+++ build-http.sh
@@ -1,3 +1,3 @@
 git rev-parse HEAD > app/tracker/revision.txt
-docker build . --rm --no-cache -t tracardi/tracardi-api:0.7.3-dev
-docker push tracardi/tracardi-api:0.7.3-dev
+docker build . --rm --no-cache -t tracardi/tracardi-api:0.7.3
+docker push tracardi/tracardi-api:0.7.3
