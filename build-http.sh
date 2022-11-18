9cc5fedf87730ed42ce41bc6e605079b1596ab55
risto <risto.kowaczewski@gmail.com>
2022-11-18 17:51:11 +0100

    Version 0.7.4-dev requirements

--- build-http.sh
+++ build-http.sh
@@ -1,3 +1,4 @@
+pip install -r requirements.dev.txt
 git rev-parse HEAD > app/tracker/revision.txt
 docker build . --rm --no-cache -t tracardi/tracardi-api:0.7.3
 docker push tracardi/tracardi-api:0.7.3
