a0c8d47b06c3b7ea42ae73a82855313f187e49d7
risto <risto.kowaczewski@gmail.com>
2022-11-18 19:47:50 +0100

    docker refactoring

--- build-http-as-latest.sh
+++ build-http-as-latest.sh
@@ -1,5 +1,5 @@
 pip install -r requirements.dev.txt
 # Uncomment in version branch. DO not use in master branch
-#git rev-parse HEAD > app/tracker/revision.txt
-#docker build . --rm --no-cache -t tracardi/tracardi-api
-#docker push tracardi/tracardi-api
+git rev-parse HEAD > app/tracker/revision.txt
+docker build . --rm --no-cache -t tracardi/tracardi-api
+docker push tracardi/tracardi-api
