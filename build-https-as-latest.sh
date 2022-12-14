2472bf2931a3f67b270567221706eeec4d1fd446
risto <risto.kowaczewski@gmail.com>
2022-12-14 18:52:53 +0100

    Version 0.8.0-dev

--- build-https-as-latest.sh
+++ build-https-as-latest.sh
@@ -1,3 +1,3 @@
-# Uncomment in version branch. DO not use in master branch
+# Uncomment in version branch. Do not use in master branch
 #docker build . --rm --no-cache -f docker.ssl.Dockerfile -t tracardi/tracardi-api-ssl
 #docker push tracardi/tracardi-api-ssl
