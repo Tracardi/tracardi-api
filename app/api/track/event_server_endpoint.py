5f01ab052e7441dfbb017f42e860184985f7a0f3
risto <risto.kowaczewski@gmail.com>
2022-12-30 10:27:50 +0100

    restore /collect

--- app/api/track/event_server_endpoint.py
+++ app/api/track/event_server_endpoint.py
@@ -1,13 +1,23 @@
 import logging
+from json import JSONDecodeError
+from typing import Optional
+
 from fastapi import APIRouter, Request, status, HTTPException
-from app.api.track.service.ip_address import get_ip_address
+
+from app.api.track.service.http import get_headers
 from tracardi.domain.api_instance import ApiInstance
+from tracardi.domain.entity import Entity
+from tracardi.domain.event_metadata import EventPayloadMetadata
+from tracardi.domain.payload.event_payload import EventPayload
+from tracardi.domain.time import Time
 from tracardi.service.tracker import track_event
 from tracardi.config import tracardi
 from tracardi.domain.payload.tracker_payload import TrackerPayload
 from tracardi.exceptions.exception import TracardiException, UnauthorizedException, FieldTypeConflictException, \
     EventValidationException
 from tracardi.exceptions.log_handler import log_handler
+from app.api.track.service.ip_address import get_ip_address
+from tracardi.service.url_constructor import url_query_params_to_dict
 
 logger = logging.getLogger(__name__)
 logger.setLevel(tracardi.logging_level)
@@ -51,4 +61,122 @@ async def _track(tracker_payload: TrackerPayload, host: str):
 async def track(tracker_payload: TrackerPayload, request: Request, profile_less: bool = False):
     tracker_payload.set_headers(dict(request.headers))
     tracker_payload.profile_less = profile_less
+    return await _track(tracker_payload, get_ip_address(request))
+
+
+@router.post("/collect/{event_type}/{source_id}/{session_id}", tags=['context-server'])
+async def track_post_webhook(event_type: str, source_id: str, request: Request, session_id: Optional[str] = None):
+    """
+    Collects data from request POST and adds event type. It stays profile-less if no session provided.
+    Session is saved when event is not profile less.
+    """
+
+    try:
+        properties = await request.json()
+    except JSONDecodeError:
+        properties = {}
+
+    tracker_payload = TrackerPayload(
+        source=Entity(id=source_id),
+        session=Entity(id=session_id),
+        metadata=EventPayloadMetadata(time=Time()),
+        context={},
+        request={
+            "headers": get_headers(request)  # it will be an event request value
+        },
+        properties={},
+        events=[
+            EventPayload(type=event_type, properties=properties)
+        ],
+        options={"saveSession": session_id is not None}
+    )
+    tracker_payload.profile_less = False
+    return await _track(tracker_payload, get_ip_address(request))
+
+
+@router.get("/collect/{event_type}/{source_id}/{session_id}", tags=['context-server'])
+async def track_get_webhook(event_type: str, source_id: str, request: Request, session_id: Optional[str] = None):
+    """
+    Collects data from request GET and adds event type. It stays profile-less if no session provided.
+    Session is saved when event is not profile less.
+    """
+    try:
+        properties = url_query_params_to_dict(request.url.query)
+    except JSONDecodeError:
+        properties = {}
+
+    tracker_payload = TrackerPayload(
+        source=Entity(id=source_id),
+        session=Entity(id=session_id),
+        metadata=EventPayloadMetadata(time=Time()),
+        context={},
+        request={
+            "headers": get_headers(request)  # it will be an event request value
+        },
+        properties={},
+        events=[
+            EventPayload(type=event_type, properties=properties)
+        ],
+        options={"saveSession": session_id is not None}
+    )
+    tracker_payload.profile_less = False
+    return await _track(tracker_payload, get_ip_address(request))
+
+
+@router.get("/collect/{event_type}/{source_id}", tags=['context-server'])
+async def track_get_webhook(event_type: str, source_id: str, request: Request):
+    """
+    Collects data from request GET and adds event type. It stays profile-less if no session provided.
+    Session is saved when event is not profile less.
+    """
+    try:
+        properties = url_query_params_to_dict(request.url.query)
+    except JSONDecodeError:
+        properties = {}
+
+    tracker_payload = TrackerPayload(
+        source=Entity(id=source_id),
+        session=None,
+        metadata=EventPayloadMetadata(time=Time()),
+        context={},
+        request={
+            "headers": get_headers(request)  # it will be an event request value
+        },
+        properties={},
+        events=[
+            EventPayload(type=event_type, properties=properties)
+        ],
+        options={"saveSession": False}
+    )
+    tracker_payload.profile_less = True
+    return await _track(tracker_payload, get_ip_address(request))
+
+
+@router.post("/collect/{event_type}/{source_id}", tags=['context-server'])
+async def track_post_webhook(event_type: str, source_id: str, request: Request):
+    """
+    Collects data from request POST and adds event type. It stays profile-less.
+    """
+
+    try:
+        properties = await request.json()
+    except JSONDecodeError:
+        properties = {}
+
+    tracker_payload = TrackerPayload(
+        source=Entity(id=source_id),
+        session=None,
+        metadata=EventPayloadMetadata(time=Time()),
+        profile=None,
+        context={},
+        request={
+            "headers": get_headers(request)  # it will be an event request value
+        },
+        properties={},
+        events=[
+            EventPayload(type=event_type, properties=properties)
+        ],
+        options={"saveSession": False}
+    )
+    tracker_payload.profile_less = True
     return await _track(tracker_payload, get_ip_address(request))
