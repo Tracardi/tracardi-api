6f981acc3ea38252a1c847b889322d7f59c59ce8
risto <risto.kowaczewski@gmail.com>
2023-02-02 13:15:49 +0100

    moved worker to tracardi

--- build-http.sh
+++ build-http.sh
@@ -1,3 +1,3 @@
 git rev-parse HEAD > app/tracker/revision.txt
 docker build . --rm --no-cache -f docker.Dockerfile -t tracardi/tracardi-api:0.8.0-dev
-docker push tracardi/tracardi-api:0.8.0-dev
+#docker push tracardi/tracardi-api:0.8.0-dev
