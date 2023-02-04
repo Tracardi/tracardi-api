a59317f44837b6c357263af870e8f2ad2a0092d3
risto <risto.kowaczewski@gmail.com>
2023-02-04 17:26:49 +0100

    Info public

--- app/api/info_endpoint.py
+++ app/api/info_endpoint.py
@@ -5,7 +5,6 @@ from tracardi.service.license import License
 
 from app.config import server
 from tracardi.config import tracardi
-from tracardi.domain.version import Version
 from tracardi.service.storage.driver import storage
 
 router = APIRouter()
@@ -19,7 +18,7 @@ async def get_version():
     return tracardi.version.version
 
 
-@router.get("/info/version/details", tags=["info"], include_in_schema=server.expose_gui_api)
+@router.get("/info/version/details", tags=["info"])
 async def get_current_backend_version():
 
     """
