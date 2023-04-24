b27e8b84bb39389b923e1a7bb1847867948e7ba9
risto <risto.kowaczewski@gmail.com>
2023-04-25 01:22:59 +0200

    Fixing errors from automated tests

--- test/api/endpoints/test_install_endpoint.py
+++ test/api/endpoints/test_install_endpoint.py
@@ -1,50 +1,49 @@
-# from test.utils import Endpoint
-#
-# endpoint = Endpoint(auth=False)
-#
-#
-# def test_should_check_if_system_installed():
-#     response = endpoint.get("/install")
-#     result = response.json()
-#     assert 'schema_ok' in result
-#     assert 'admin_ok' in result
-#
-#
-# def test_should_not_install_system():
-#     response = endpoint.post("/install", data={
-#         "username": 'a@a.pl',
-#         "password": "a",
-#         "token": "",
-#         "needs_admin": True
-#     })
-#     assert response.status_code == 403
-#     result = response.json()
-#     assert 'created' not in result
-#     assert 'plugins' not in result
-#     assert 'admin' not in result
-#
-#
-# def test_should_install_system():
-#     response = endpoint.post("/install", data={
-#         "username": 'a@a.pl',
-#         "password": "a",
-#         "token": "tracardi",
-#         "needs_admin": True
-#     })
-#     result = response.json()
-#     assert 'created' in result
-#     assert 'templates' in result['created']
-#     assert 'indices' in result['created']
-#     assert 'aliases' in result['created']
-#     assert 'plugins' in result
-#     assert 'admin' in result
-#     assert isinstance(result['admin'], bool)
-#
-# def test_should_install_system_plugins():
-#     response = endpoint.get("/install/plugins")
-#     result = response.json()
-#     assert 'registered' in result
-#     assert isinstance(result['registered'], list)
-#     assert 'tracardi.process_engine.action.v1.flow.start.start_action' in result['registered']
-#
-#
+from test.utils import Endpoint
+
+endpoint = Endpoint(auth=False)
+
+
+def test_should_check_if_system_installed():
+    response = endpoint.get("/install")
+    result = response.json()
+    assert 'schema_ok' in result
+    assert 'admin_ok' in result
+
+
+def test_should_not_install_system():
+    response = endpoint.post("/install", data={
+        "username": 'a@a.pl',
+        "password": "a",
+        "token": "",
+        "needs_admin": True
+    })
+    assert response.status_code == 403
+    result = response.json()
+    assert 'created' not in result
+    assert 'plugins' not in result
+    assert 'admin' not in result
+
+
+def test_should_install_system():
+    response = endpoint.post("/install", data={
+        "username": 'a@a.pl',
+        "password": "a",
+        "token": "tracardi",
+        "needs_admin": True
+    })
+    result = response.json()
+    assert 'created' in result[0]  # Staging env
+    assert 'templates' in result[0]['created']
+    assert 'indices' in result[0]['created']
+    assert 'aliases' in result[0]['created']
+    assert 'plugins' in result[0]
+    assert 'admin' in result[0]
+    assert isinstance(result[0]['admin'], bool)
+
+
+def test_should_install_system_plugins():
+    response = endpoint.get("/install/plugins")
+    result = response.json()
+    assert 'registered' in result
+    assert isinstance(result['registered'], list)
+    assert 'tracardi.process_engine.action.v1.flow.start.start_action' in result['registered']
