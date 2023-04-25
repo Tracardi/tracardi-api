2cc66768abbab07c4d94d7f0e76312feb8091bdc
risto <risto.kowaczewski@gmail.com>
2023-04-25 23:37:39 +0200

    Version change to 0.8.2-dev

--- build-http-k8s.sh
+++ build-http-k8s.sh
@@ -1,3 +1,3 @@
 git rev-parse HEAD > app/tracker/revision.txt
-docker build . --rm -f docker.k8s.Dockerfile -t tracardi/tracardi-api-k8s:0.8.1-dev
-docker push tracardi/tracardi-api-k8s:0.8.1-dev
+docker build . --rm -f docker.k8s.Dockerfile -t tracardi/tracardi-api-k8s:0.8.2-dev
+docker push tracardi/tracardi-api-k8s:0.8.2-dev
--- build-http.sh
+++ build-http.sh
@@ -1,3 +1,3 @@
 git rev-parse HEAD > app/tracker/revision.txt
-docker build . --rm --no-cache -f docker.Dockerfile -t tracardi/tracardi-api:0.8.1-dev
-docker push tracardi/tracardi-api:0.8.1-dev
+docker build . --rm --no-cache -f docker.Dockerfile -t tracardi/tracardi-api:0.8.2-dev
+docker push tracardi/tracardi-api:0.8.2-dev
