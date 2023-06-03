0a8b699b18859891944258c02180e00f5eadd569
risto <risto.kowaczewski@gmail.com>
2023-06-03 21:47:29 +0200

    Multi tenant

--- app/api/migration_endpoint.py
+++ app/api/migration_endpoint.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 
 from fastapi import APIRouter, Depends, HTTPException
+from tracardi.context import get_context
 
 from tracardi.service.storage.driver import storage
 from tracardi.service.storage.indices_manager import check_indices_mappings_consistency
@@ -101,12 +102,13 @@ async def run_migration(migration: MigrationPayload):
 
 
 @router.get("/migration/{from_version}", tags=["migration"], include_in_schema=server.expose_gui_api)
-async def get_migration_schemas(from_version: str, from_prefix: Optional[str] = None):
+async def get_migration_schemas(from_version: str):
+    print(get_context().tenant)
     try:
         manager = MigrationManager(
             from_version=from_version,
             to_version=tracardi.version.version,
-            from_prefix=from_prefix,
+            from_prefix=get_context().tenant,
             to_prefix=tracardi.version.name
         )
         return await manager.get_customized_schemas()
