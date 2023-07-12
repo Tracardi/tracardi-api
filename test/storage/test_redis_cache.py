74de6c77996ab3a1f67e96fa4b2fa7156fa60f0a
risto <risto.kowaczewski@gmail.com>
2023-07-12 23:58:04 +0200

    Tests fixes

--- test/storage/test_redis_cache.py
+++ test/storage/test_redis_cache.py
@@ -1,39 +1,41 @@
 from time import sleep
 
+from tracardi.context import ServerContext, Context
 from tracardi.service.storage.redis.cache import RedisCache
 
 
 def test_redis_cache():
-    value = {
-        "a": {
-            "b": [1, 2, 3]
-        },
-        "c": {
-            "d": None
+    with ServerContext(Context(production=False)):
+        value = {
+            "a": {
+                "b": [1, 2, 3]
+            },
+            "c": {
+                "d": None
+            }
         }
-    }
-    rc = RedisCache(ttl=1, prefix="pytest:")
+        rc = RedisCache(ttl=1, prefix="pytest:")
 
-    if "key" not in rc:
-        assert rc["key"] is None
-    else:
+        if "key" not in rc:
+            assert rc["key"] is None
+        else:
+            del rc["key"]
+
+        rc["key"] = value
+
+        assert rc["key"] == value
+
+        # Can be deleted
+
+        del rc['Not-exists']
+
         del rc["key"]
+        assert rc["key"] is None
 
-    rc["key"] = value
+        # Expires
 
-    assert rc["key"] == value
+        rc["key"] = value
 
-    # Can be deleted
+        sleep(2)
 
-    del rc['Not-exists']
-
-    del rc["key"]
-    assert rc["key"] is None
-
-    # Expires
-
-    rc["key"] = value
-
-    sleep(2)
-
-    assert rc["key"] is None
+        assert rc["key"] is None
--- test/storage/test_token_db.py
+++ test/storage/test_token_db.py
@@ -1,23 +1,25 @@
 from uuid import uuid4
 
 from app.api.auth.user_db import TokenDb
+from tracardi.context import ServerContext, Context
 from tracardi.domain.user import User
 
 
 def test_user_set_delete():
-    token2user = TokenDb()
-    user = User(id=str(uuid4()),
-                password="pass",
-                full_name="Name",
-                email="test@test.pl",
-                roles=['admin']
-                )
-    token = token2user.set(user)
-    user_cache = token2user.get(token)
-    assert user_cache.email == user.email
-    assert user_cache.id == user.id
+    with ServerContext(Context(production=False)):
+        token2user = TokenDb()
+        user = User(id=str(uuid4()),
+                    password="pass",
+                    full_name="Name",
+                    email="test@test.pl",
+                    roles=['admin']
+                    )
+        token = token2user.set(user)
+        user_cache = token2user.get(token)
+        assert user_cache.email == user.email
+        assert user_cache.id == user.id
 
-    token2user.delete(token)
+        token2user.delete(token)
 
-    user_cache = token2user.get(token)
-    assert user_cache is None
+        user_cache = token2user.get(token)
+        assert user_cache is None
