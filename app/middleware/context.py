35ae37a514a2db727f93793631b7c65fa2e266f1
risto <risto.kowaczewski@gmail.com>
2023-07-25 22:57:24 +0200

    Tenant management

--- app/middleware/context.py
+++ app/middleware/context.py
@@ -36,10 +36,16 @@ def _get_context_object(scope) -> Context:
     if tenant is None:
         raise OSError(f"Can not find tenant for this URL. Reason: Hostname `{hostname}` must have 3 parts.")
 
-    if len(tenant) < 3 or tenant.isnumeric():
+    if len(tenant) < 3:
         raise OSError(f"Tenant name {tenant} is not correct. "
-                      f"Reason: Tenant name can not be shorted then 3 letters "
-                      f"and must not be numbers. Your system is set-up to support multi-tenancy "
+                      f"Reason: Tenant name can not be shorted then 3 letters. "
+                      f"Your system is set-up to support multi-tenancy "
+                      f"that means access only through domain name is available.")
+
+    if tenant.isnumeric():
+        raise OSError(f"Tenant name {tenant} is not correct. "
+                      f"Reason: Tenant name must not be a number. "
+                      f"Your system is set-up to support multi-tenancy "
                       f"that means access only through domain name is available.")
 
     if not production:  # Staging as default
