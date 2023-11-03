c106ffaed1dbc8274cbb04a4ecb38825651ac28e
risto <risto.kowaczewski@gmail.com>
2023-11-03 10:56:39 +0100

    Cleanup in Demoe event sources

--- app/api/install_endpoint.py
+++ app/api/install_endpoint.py
@@ -1,4 +1,3 @@
-import datetime
 import logging
 import os
 from typing import Optional
@@ -10,16 +9,9 @@ from tracardi.service.installation import install_system, check_installation
 from tracardi.service.tracker import track_event
 from tracardi.config import tracardi
 from tracardi.domain.credentials import Credentials
-from tracardi.domain.event_source import EventSource
-from tracardi.domain.named_entity import NamedEntity
 from tracardi.exceptions.log_handler import log_handler
 from tracardi.service.fake_data_maker.generate_payload import generate_payload
 from tracardi.service.plugin.plugin_install import install_default_plugins
-from tracardi.service.setup.data.defaults import open_rest_source_bridge
-from tracardi.service.setup.setup_indices import add_ids
-from tracardi.service.storage.driver.elastic import raw as raw_db
-from tracardi.service.storage.driver.elastic import event_source as event_source_db
-from tracardi.service.storage.index import Resource
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
@@ -43,28 +35,10 @@ async def install_plugins():
 @router.get("/install/demo", tags=["installation"], include_in_schema=tracardi.expose_gui_api)
 async def install_demo_data():
     # Demo
-    if os.environ.get("DEMO", None) == 'yes':
-
-        event_source = EventSource(
-            id=open_rest_source_bridge.id,
-            type=["internal"],
-            name="Test random data",
-            channel="Internal",
-            description="Internal event source for random data.",
-            bridge=NamedEntity(**open_rest_source_bridge.model_dump()),
-            timestamp=datetime.datetime.utcnow(),
-            tags=["internal"],
-            groups=["Internal"]
-        )
-
-        await raw_db.bulk_upsert(
-            Resource().get_index_constant('event-source').get_write_index(),
-            list(add_ids([event_source.model_dump()])))
-
-        await event_source_db.refresh()
+    if os.environ.get("DEMO", 'no') == 'yes':
 
         for i in range(0, 10):
-            payload = generate_payload(source=open_rest_source_bridge.id)
+            payload = generate_payload(source=tracardi.demo_source)
 
             await track_event(
                 TrackerPayload(**payload),
