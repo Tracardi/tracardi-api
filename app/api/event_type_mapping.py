6016a5ea4da2b30a43d7cfc80db061c18f96a01f
risto <risto.kowaczewski@gmail.com>
2023-11-27 13:55:59 +0100

    Migration to mysql

--- app/api/event_type_mapping.py
+++ app/api/event_type_mapping.py
@@ -1,17 +1,15 @@
-import asyncio
 from uuid import uuid4
 
 from fastapi import APIRouter, Depends, HTTPException
 
 from app.api.auth.permissions import Permissions
 from tracardi.config import tracardi
-from app.service.grouping import group_records
 from tracardi.domain.event_type_metadata import EventTypeMetadata
-from tracardi.domain.value_object.bulk_insert_result import BulkInsertResult
 from tracardi.service.events import get_default_mappings_for
-from tracardi.service.storage.driver.elastic import event_management as event_management_db
-from tracardi.service.storage.driver.elastic import event as event_db
-from typing import Optional
+from typing import Optional, List
+
+from tracardi.service.storage.mysql.mapping.event_to_event_mapping import map_to_event_mapping
+from tracardi.service.storage.mysql.service.event_mapping_service import EventMappingService
 
 router = APIRouter(
     dependencies=[Depends(Permissions(roles=["admin", "developer"]))],
@@ -19,54 +17,39 @@ router = APIRouter(
 )
 
 
-@router.put("/mapping/refresh", tags=["event-type"], include_in_schema=tracardi.expose_gui_api,
-            response_model=dict)
-async def refresh_event_type_mapping():
-    """
-    Refreshes event type mapping
-    """
-    return await event_management_db.refresh()
-
-
-@router.post("/mapping", tags=["event-type"], include_in_schema=tracardi.expose_gui_api,
-             response_model=BulkInsertResult)
+@router.post("/mapping", tags=["event-type"], include_in_schema=tracardi.expose_gui_api)
 async def add_event_type_mapping(event_mapping: EventTypeMetadata):
     """
     Creates new event type mapping in database
     """
 
-    # Save tags
-    result = await event_management_db.save(event_mapping)
-    await event_management_db.refresh()
+    ems = EventMappingService()
+    return await ems.insert(event_mapping)
 
-    if result.errors:
-        raise ValueError(result.errors)
-
-    # Update events for new tags in background
-
-    task = event_db.update_tags(
-        event_type=event_mapping.event_type,
-        tags=event_mapping.tags)
-
-    asyncio.create_task(task)
-
-    return result
+    # # Save tags
+    # result = await event_management_db.save(event_mapping)
+    # await event_management_db.refresh()
+    #
+    # if result.errors:
+    #     raise ValueError(result.errors)
+    #
+    # return result
 
 
 @router.get("/mappings/{event_type}",
             tags=["event-type"],
             include_in_schema=tracardi.expose_gui_api,
             response_model=dict)
-async def list_event_type_metadata(event_type: str):
+async def list_event_mappings(event_type: str):
     """
     Returns a list of event type mappings both build-in and custom for given event type
     """
 
-    records = []
+    mappings: List[EventTypeMetadata] = []
 
     build_in = get_default_mappings_for(event_type, "copy")
     if build_in is not None:
-        build_in = {
+        build_in = EventTypeMetadata(**{
             'id': str(uuid4()),
             'name': 'Build in mapping',
             'event_type': event_type, 'description': f"\"{event_type}\" event mapping.",
@@ -74,13 +57,16 @@ async def list_event_type_metadata(event_type: str):
             'index_schema': build_in,
             'tags': ['General'],
             'build_in': True
-        }
-        records.append(build_in)
+        })
+        mappings.append(build_in)
 
-    record = await event_management_db.get_event_type_mapping(event_type)
-    if record is not None:
-        record['build-in'] = False
-        records.append(record)
+    ems = EventMappingService()
+    records = await ems.load_by_event_type(event_type)
+
+    # record = await event_management_db.get_event_type_mapping(event_type)
+    if records.exists():
+        for record in records.map_to_objects(map_to_event_mapping):
+            mappings.append(record)
 
     if not records:
         raise HTTPException(status_code=404, detail=f"Mapping for event type [{event_type}] not found.")
@@ -91,40 +77,47 @@ async def list_event_type_metadata(event_type: str):
     }
 
 
-@router.get("/mapping/{event_type}",
+@router.get("/mapping/{event_type_id}",
             tags=["event-type"],
             include_in_schema=tracardi.expose_gui_api,
-            response_model=Optional[dict])
-async def get_event_type_mapping(event_type: str):
+            response_model=Optional[EventTypeMetadata])
+async def get_event_mapping_by_id(event_type_id: str):
     """
     Return custom event type mapping for given event type
     """
 
-    record = await event_management_db.get_event_type_mapping(event_type)
+    ems = EventMappingService()
+    record =  await ems.load_by_id(event_type_id)
 
-    if not record:
-        raise HTTPException(status_code=404, detail=f"Mapping for event type [{event_type}] not found.")
+    if not record.exists():
+        raise HTTPException(status_code=404, detail=f"Mapping for event type [{event_type_id}] not found.")
 
-    return record
+    return record.map_to_object(map_to_event_mapping)
 
 
-@router.delete("/mapping/{event_type}", tags=["event-type"], include_in_schema=tracardi.expose_gui_api,
+@router.delete("/mapping/{event_type_id}", tags=["event-type"], include_in_schema=tracardi.expose_gui_api,
                response_model=dict)
-async def del_event_type_metadata(event_type: str):
+async def del_event_type_metadata(event_type_id: str):
     """
     Deletes event type metadata for given event type
     """
-    result = await event_management_db.del_event_type_mapping(event_type)
-    await event_management_db.refresh()
-
-    return {"deleted": 1 if result is not None and result["result"] == "deleted" else 0}
+    ems = EventMappingService()
+    return await ems.delete_by_id(event_type_id)
 
 
 @router.get("/search/mappings", tags=["event-type"], include_in_schema=tracardi.expose_gui_api,
             response_model=dict)
-async def list_event_type_mappings_by_tag(query: str = None, start: Optional[int] = 0, limit: Optional[int] = 200):
+async def list_event_type_mappings_by_tag(query: str = None, start: Optional[int] = None, limit: Optional[int] = 200):
     """
     Lists event type metadata by tag, according to given start (int), limit (int) and query (str)
     """
-    result = await event_management_db.load_events_type_mapping(start, limit)
-    return group_records(result, query, group_by='tags', search_by='name', sort_by='name')
+
+    ems = EventMappingService()
+    records = await ems.load_all(search=query, limit=limit, offset=start)
+
+    return {
+        "total": records.count(),
+        "grouped": {
+            "Event mappings": list(records.map_to_objects(map_to_event_mapping))
+        }
+    }
