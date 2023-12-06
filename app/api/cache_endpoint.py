58324ff763d13aac96144d5df604ae16d5e8922b
risto <risto.kowaczewski@gmail.com>
2023-12-07 00:24:25 +0100

    Cache redo.

---
+++ app/api/cache_endpoint.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends
+from tracardi.config import tracardi
+from tracardi.context import get_context
+from tracardi.service.storage.redis.cache import RedisCache
+from tracardi.service.tracking.cache.profile_cache import get_profile_key_namespace
+from tracardi.service.tracking.cache.session_cache import get_session_key_namespace
+from .auth.permissions import Permissions
+
+router = APIRouter(
+    dependencies=[Depends(Permissions(roles=["admin", "developer","marketer"]))]
+)
+redis_cache = RedisCache(ttl=None)
+
+
+@router.get("/cache/profile/expire", tags=["cache"], include_in_schema=tracardi.expose_gui_api)
+async def get_profile_cache_ttl(profile_id: str):
+    """
+    Returns cache expiration data
+    """
+    namespace = get_profile_key_namespace(profile_id, get_context())
+    return {"ttl" : redis_cache.get_ttl(profile_id, namespace)}
+
+
+@router.get("/cache/session/expire", tags=["cache"], include_in_schema=tracardi.expose_gui_api)
+async def get_session_cache_ttl(session_id: str):
+    """
+    Returns cache expiration data
+    """
+    namespace = get_session_key_namespace(session_id, get_context())
+    return {"ttl" : redis_cache.get_ttl(session_id, namespace)}
