1f69dda91c97f6bbf906412b3c280ea7ea408282
risto <risto.kowaczewski@gmail.com>
2023-12-24 13:54:00 +0100

    Remove task index

--- app/api/task_endpoint.py
+++ app/api/task_endpoint.py
@@ -1,76 +1,88 @@
 from fastapi import APIRouter, Depends
 
-from tracardi.service.storage.driver.elastic import task as task_db
+from tracardi.service.storage.mysql.mapping.task_mapping import map_to_task
+from tracardi.service.storage.mysql.service.task_service import BackgroundTaskService
 from .auth.permissions import Permissions
 from tracardi.config import tracardi
 from tracardi.domain.task import Task
+from ..service.grouping import get_grouped_result
 
 router = APIRouter(
     dependencies=[Depends(Permissions(roles=["admin", "developer"]))]
 )
 
+bts = BackgroundTaskService()
 
 @router.get("/tasks", tags=["task"], include_in_schema=tracardi.expose_gui_api)
-async def load_tasks(query: str = None, limit: int = 50):
-    if not query:
-        query = {
-            "match_all": {}
-        }
-    else:
-        query = {
-            "wildcard": {
-                "name": f"*{query}*"
-            }
-        }
+async def load_tasks(query: str = None, start:int = 0, limit: int = 100):
 
-    result = await task_db.load_tasks(query, limit=limit)
-    return {
-        "grouped": {
-            "Tasks": list(result)
-        }
-    }
+    records = await bts.load_all(search=query, offset=start, limit=limit)
+    return get_grouped_result("Tasks", records, map_to_task)
+
+    # if not query:
+    #     query = {
+    #         "match_all": {}
+    #     }
+    # else:
+    #     query = {
+    #         "wildcard": {
+    #             "name": f"*{query}*"
+    #         }
+    #     }
+    #
+    # result = await task_db.load_tasks(query, limit=limit)
+    # return {
+    #     "grouped": {
+    #         "Tasks": list(result)
+    #     }
+    # }
 
 
 @router.get("/tasks/type/{type}", tags=["task"], include_in_schema=tracardi.expose_gui_api)
-async def load_tasks_by_type(type: str, query: str = None, limit: int = 50):
+async def load_tasks_by_type(type: str, query: str = None, start:int = 0, limit: int = 100):
 
     """Returns tasks of a given type"""
 
-    body = {
-        "bool": {
-            "must": [
-                {
-                    "term": {
-                        "type": type
-                    }
-                }
-            ]
-        }
-    }
+    records = await bts.load_all_by_type(type, search=query, offset=start, limit=limit)
+    return get_grouped_result("Tasks", records, map_to_task)
 
-    if query:
-        body['bool']['must'].append({
-            "wildcard": {
-                "name": f"*{query}*"
-            }
-        })
-
-    result = await task_db.load_tasks(body, limit=limit)
-
-    return {
-        "grouped": {
-            "Tasks": list(result)
-        }
-    }
+    # body = {
+    #     "bool": {
+    #         "must": [
+    #             {
+    #                 "term": {
+    #                     "type": type
+    #                 }
+    #             }
+    #         ]
+    #     }
+    # }
+    #
+    # if query:
+    #     body['bool']['must'].append({
+    #         "wildcard": {
+    #             "name": f"*{query}*"
+    #         }
+    #     })
+    #
+    # result = await task_db.load_tasks(body, limit=limit)
+    #
+    # return {
+    #     "grouped": {
+    #         "Tasks": list(result)
+    #     }
+    # }
 
 
 @router.delete("/task/{id}", tags=["task"], include_in_schema=tracardi.expose_gui_api)
 async def delete_task(id: str):
-    return await task_db.delete_task(id)
+    return await bts.delete_by_id(id)
+    # return await task_db.delete_task(id)
 
 
 @router.post("/task", tags=["task"], include_in_schema=tracardi.expose_gui_api)
 async def upsert_task(task: Task):
-    result = await task_db.upsert_task(task)
-    await task_db.refresh()
-    return result
+    return await bts.insert(task)
+    # result = await task_db.upsert_task(task)
+    # await task_db.refresh()
+    # return result
