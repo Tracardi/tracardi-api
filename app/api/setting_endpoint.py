c13a6c5f04da124875d2e69c2c66cdf18dde5d46
risto <risto.kowaczewski@gmail.com>
2023-12-31 01:20:20 +0100

    Final remove of setting index

--- app/api/setting_endpoint.py
+++ app/api/setting_endpoint.py
@@ -2,16 +2,19 @@ from fastapi import APIRouter
 from fastapi import HTTPException, Depends
 
 from tracardi.domain.setting import Setting
+from tracardi.service.storage.mysql.map_to_named_entity import map_to_named_entity
+from tracardi.service.storage.mysql.mapping.setting_mapping import map_to_setting
+from tracardi.service.storage.mysql.service.setting_service import SettingService
 from .auth.permissions import Permissions
 from tracardi.config import tracardi
-from ..service.grouping import group_records
-from tracardi.service.storage.driver.elastic import setting as setting_db
+from ..service.grouping import get_result_dict, get_grouped_result
 from typing import Optional
-
 
 router = APIRouter(
     dependencies=[Depends(Permissions(roles=["admin", "developer", "marketer"]))]
 )
+
+ss = SettingService()
 
 
 @router.get("/settings/{type}/entities", tags=["setting"], include_in_schema=tracardi.expose_gui_api)
@@ -19,7 +22,10 @@ async def load_setting_entities(type: str):
     """
     Returns list of setting as named entities.
     """
-    return {"result": [dict(id=report.id, name=report.name) for report in await setting_db.load_all(type)]}
+
+    records = await ss.load_all()
+
+    return get_result_dict(records, map_to_named_entity)
 
 
 @router.get("/setting/{type}/{id}", tags=["setting"], include_in_schema=tracardi.expose_gui_api)
@@ -27,12 +33,13 @@ async def get_setting(type: str, id: str):
     """
     Returns setting with given ID.
     """
-    result = await setting_db.load(type, id)
 
-    if result is None:
-        raise HTTPException(status_code=404, detail=f"Report with ID {id} not found.")
+    result = await ss.load_by_id(id)
 
-    return result
+    if not result.exists():
+        raise HTTPException(status_code=404, detail=f"Setting with ID {id} not found.")
+
+    return result.map_to_object(map_to_setting)
 
 
 @router.get("/settings/{type}", tags=["setting"], include_in_schema=tracardi.expose_gui_api)
@@ -40,8 +47,9 @@ async def load_grouped_settings(type: str, query: Optional[str] = None):
     """
     Returns list of settings according to given query, grouped by tag.
     """
-    result = await setting_db.load_for_grouping(type, query)
-    return group_records(result, None)
+
+    records = await ss.load_all(search=query, limit=100)
+    return get_grouped_result("Metrics", records, map_to_setting)
 
 
 @router.post("/setting/{type}", tags=["setting"], include_in_schema=tracardi.expose_gui_api)
@@ -49,12 +57,8 @@ async def add_setting(setting: Setting):
     """
     Adds or edits setting in the database.
     """
-    if setting.type == type:
-        raise TypeError("Incorrect type.")
 
-    result = await setting_db.upsert(setting)
-    await setting_db.refresh()
-    return result
+    return await ss.insert(setting)
 
 
 @router.delete("/setting/{type}/{id}", tags=["setting"], include_in_schema=tracardi.expose_gui_api)
@@ -62,6 +66,5 @@ async def delete_setting(id: str):
     """
     Deletes setting from the database
     """
-    result = await setting_db.delete(id)
-    await setting_db.refresh()
-    return result
+
+    return await ss.delete_by_id(id)
