b1524d130c4327da7c039ff423fe350f2292f071
risto <risto.kowaczewski@gmail.com>
2024-02-06 00:18:49 +0100

    feat(api): add customer consent handling
    This commit adds the functionality to handle customer consent in the customer_endpoint.py file.
    
    The changes include:
    - Importing necessary modules and classes from the tracardi service and storage packages.
    - Implementing a RedisClient to interact with Redis.
    - Creating a profile_key and profile_lock for locking the profile during consent handling.
    - Loading the session, source, and profile from their respective storage.
    - Checking if the source, profile, and session exist, and raising an HTTPException if they don't.
    - Handling different scenarios for adding consent based on the 'all' flag.
    - Updating the profile's consents based on the 'all' flag and the provided consents.
    - Setting the 'granted' key in the profile's aux dictionary to True.
    - Saving the profile cache and returning the saved profile.
    
    This commit enables the addition of customer consent and ensures that the profile is updated accordingly.

--- app/api/customer_endpoint.py
+++ app/api/customer_endpoint.py
@@ -4,7 +4,10 @@ from typing import Optional
 from fastapi import APIRouter, HTTPException
 from pytimeparse.timeparse import timeparse
 
+from tracardi.service.storage.redis.collections import Collection
+from tracardi.service.storage.redis_client import RedisClient
 from tracardi.service.tracking.cache.profile_cache import save_profile_cache
+from tracardi.service.tracking.locking import Lock, async_mutex
 from tracardi.service.tracking.storage.profile_storage import load_profile, save_profile
 from tracardi.service.tracking.storage.session_storage import load_session
 from tracardi.service.utils.date import now_in_utc
@@ -13,7 +16,7 @@ from tracardi.domain.payload.customer_consent import CustomerConsent
 from tracardi.domain.profile import ConsentRevoke
 from tracardi.service.storage.driver.elastic import event_source as event_source_db
 from tracardi.service.storage.driver.elastic import consent_type as consent_type_db
-
+from tracardi.service.utils.getters import get_entity_id
 
 router = APIRouter()
 
@@ -23,39 +26,48 @@ async def add_consent_type(data: CustomerConsent, all: Optional[bool] = False):
     """
     Adds customer consent
     """
+
     session = await load_session(data.session.id)
-    profile = await load_profile(data.profile.id)
     source = await event_source_db.load(data.source.id)
 
-    if not source or not profile or not session:
-        raise HTTPException(status_code=403, detail="Access denied")
+    _redis = RedisClient()
+    profile_key = Lock.get_key(Collection.lock_tracker, "profile", get_entity_id(data.profile))
+    profile_lock = Lock(_redis, profile_key, default_lock_ttl=3)
 
-    if all:
-        for consent in await consent_type_db.load_all():
-            consent_type = ConsentType(**consent)
 
-            if consent_type.auto_revoke:
-                try:
-                    seconds = timeparse(consent_type.auto_revoke)
-                    now = now_in_utc()
-                    revoke = now + timedelta(seconds=seconds)
-                    revoke = ConsentRevoke(revoke=revoke)
-                except Exception:
+    async with async_mutex(profile_lock, name='add_consent_type'):
+
+        profile = await load_profile(data.profile.id)
+
+        if not source or not profile or not session:
+            raise HTTPException(status_code=403, detail="Access denied")
+
+        if all:
+            for consent in await consent_type_db.load_all():
+                consent_type = ConsentType(**consent)
+
+                if consent_type.auto_revoke:
+                    try:
+                        seconds = timeparse(consent_type.auto_revoke)
+                        now = now_in_utc()
+                        revoke = now + timedelta(seconds=seconds)
+                        revoke = ConsentRevoke(revoke=revoke)
+                    except Exception:
+                        revoke = ConsentRevoke()
+
+                else:
                     revoke = ConsentRevoke()
 
-            else:
-                revoke = ConsentRevoke()
+                profile.consents[consent['id']] = revoke
+        else:
+            for consent, flag in data.consents.items():
+                if flag:
+                    profile.consents[consent] = ConsentRevoke()
+                else:
+                    if consent in profile.consents:
+                        del profile.consents[consent]
 
-            profile.consents[consent['id']] = revoke
-    else:
-        for consent, flag in data.consents.items():
-            if flag:
-                profile.consents[consent] = ConsentRevoke()
-            else:
-                if consent in profile.consents:
-                    del profile.consents[consent]
+        profile.aux['consents'] = {"granted": True}
 
-    profile.aux['consents'] = {"granted": True}
-
-    save_profile_cache(profile)
-    return await save_profile(profile)
+        save_profile_cache(profile)
+        return await save_profile(profile)
