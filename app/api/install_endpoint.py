4a8157d1dbfaa76b0888327098db44539f39be58
risto <risto.kowaczewski@gmail.com>
2024-02-07 19:16:06 +0100

    Lock auto index creation

--- app/api/install_endpoint.py
+++ app/api/install_endpoint.py
@@ -1,18 +1,12 @@
-import os
 from typing import Optional
 from fastapi import APIRouter, HTTPException
 
-from tracardi.domain.payload.tracker_payload import TrackerPayload
 from tracardi.service.installation import install_system, check_installation
-from tracardi.service.tracker import track_event
 from tracardi.config import tracardi
 from tracardi.domain.credentials import Credentials
-from tracardi.exceptions.log_handler import get_logger
-from tracardi.service.fake_data_maker.generate_payload import generate_payload
 from tracardi.service.plugin.plugin_install import install_default_plugins
 
 router = APIRouter()
-logger = get_logger(__name__)
 
 
 @router.get("/install", tags=["installation"], include_in_schema=tracardi.expose_gui_api, response_model=dict)
@@ -26,20 +20,6 @@ async def check_if_installation_complete():
 @router.get("/install/plugins", tags=["installation"], include_in_schema=tracardi.expose_gui_api, response_model=dict)
 async def install_plugins():
     return await install_default_plugins()
-
-
-@router.get("/install/demo", tags=["installation"], include_in_schema=tracardi.expose_gui_api)
-async def install_demo_data():
-    # Demo
-    if os.environ.get("DEMO", 'no') == 'yes':
-
-        for i in range(0, 10):
-            payload = generate_payload(source=tracardi.demo_source)
-
-            await track_event(
-                TrackerPayload(**payload),
-                "0.0.0.0",
-                allowed_bridges=['internal'])
 
 
 @router.post("/install", tags=["installation"], include_in_schema=tracardi.expose_gui_api)
