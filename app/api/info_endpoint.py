968cb754e925cbe3fc0c456d317178b461e86704
risto <risto.kowaczewski@gmail.com>
2024-02-12 10:02:12 +0100

    feat(api): Add endpoint to get info about Tracardi Installed Versions
    Add new endpoint `/info/versions` to retrieve information about the Tracardi installed versions. This endpoint returns a JSON response containing the versions grouped by the version type.
    
    The implementation includes the following changes:
    - Added import statement for the `get_grouped_result` function from the `service.grouping` module.
    - Added import statements for the `map_to_version` function and `VersionService` from the `tracardi.service.storage.mysql.mapping.version_mapping` and `tracardi.service.storage.mysql.service.version_service` modules, respectively.
    - Defined a new route handler function `get_versions` for the `/info/versions` endpoint. This function retrieves all version records from the database using the `VersionService.load_all()` method, and then uses the `get_grouped_result` function to group the versions by version type.
    - Registered the `get_versions` function as a route handler for the `/info/versions

--- app/api/info_endpoint.py
+++ app/api/info_endpoint.py
@@ -2,13 +2,27 @@ from datetime import datetime
 
 from fastapi import APIRouter
 
+from service.grouping import get_grouped_result
 from tracardi.domain.api_instance import ApiInstance
 from tracardi.domain.installation_status import installation_status
 from tracardi.service.license import License
 
 from tracardi.config import tracardi
+from tracardi.service.storage.mysql.mapping.version_mapping import map_to_version
+from tracardi.service.storage.mysql.service.version_service import VersionService
 
 router = APIRouter()
+
+
+@router.get("/info/versions", tags=["info"], include_in_schema=tracardi.expose_gui_api)
+async def get_versions():
+    """
+    Returns info about Tracardi Installed Versions
+    """
+
+    vs = VersionService()
+    records = await vs.load_all(limit=100)
+    return get_grouped_result("Versions", records, map_to_version)
 
 
 @router.get("/info/version", tags=["info"], include_in_schema=tracardi.expose_gui_api, response_model=str)
@@ -30,7 +44,6 @@ async def get_current_backend_version():
     version = tracardi.version.model_dump(mode='json')
     version['instance'] = ApiInstance().id
     version['installed'] = await installation_status.get_status()
-
 
     if License.has_license():
         license = License.check()
