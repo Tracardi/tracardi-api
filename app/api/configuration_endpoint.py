b0744769474d0d13ed0658f224ecac0de1b15dab
risto <risto.kowaczewski@gmail.com>
2024-02-16 09:53:15 +0100

    Changes for GUI

--- app/api/configuration_endpoint.py
+++ app/api/configuration_endpoint.py
@@ -3,9 +3,9 @@ from typing import Optional
 from fastapi import APIRouter, Depends, HTTPException
 
 from app.service.grouping import get_grouped_result
-from tracardi.domain.test import Test
-from tracardi.service.storage.mysql.mapping.test_mapping import map_to_test
-from tracardi.service.storage.mysql.service.test_service import TestService
+from tracardi.domain.configuration import Configuration
+from tracardi.service.storage.mysql.mapping.configuration_mapping import map_to_configuration
+from tracardi.service.storage.mysql.service.configuration_service import ConfigurationService
 
 from app.api.auth.permissions import Permissions
 from tracardi.config import tracardi
@@ -14,32 +14,32 @@ router = APIRouter(
     dependencies=[Depends(Permissions(roles=["admin", "maintainer", "developer"]))]
 )
 
-ts = TestService()
+cs = ConfigurationService()
 
 @router.get("/configuration/{id}", tags=["deployment"], include_in_schema=tracardi.expose_gui_api)
 async def get_test(id: str):
-    record = await ts.load_by_id(id)
+    record = await cs.load_by_id(id)
     if not record.exists():
         raise HTTPException(status_code=404, detail=f"Configuration with ID {id} not found.")
 
-    return record.map_to_object(map_to_test)
+    return record.map_to_object(map_to_configuration)
 
 
 @router.get("/configuration", tags=["deployment"], include_in_schema=tracardi.expose_gui_api)
-async def list_tests(query: Optional[str] = None, limit: int = 200):
-    records = await ts.load_all(search=query, limit=limit)
+async def list_configuration(query: Optional[str] = None, limit: int = 200):
+    records = await cs.load_all(search=query, limit=limit)
 
-    return get_grouped_result("Configuration Settings", records, map_to_test)
+    return get_grouped_result("Configuration Settings", records, map_to_configuration)
 
 
 @router.post("/configuration", tags=["deployment"], include_in_schema=tracardi.expose_gui_api)
-async def add_test(test: Test):
-    return await ts.upsert(test)
+async def add_test(config: Configuration):
+    return await cs.upsert(config)
 
 
 @router.delete("/configuration/{id}", tags=["report"], include_in_schema=tracardi.expose_gui_api)
 async def delete_test(id: str):
     """
-    Deletes test from the database
+    Deletes configuration from the database
     """
-    return await ts.delete_by_id(id)
+    return await cs.delete_by_id(id)
