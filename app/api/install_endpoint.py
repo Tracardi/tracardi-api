31eefd210c663e20cfabbc2df0f045f7875f0f5f
risto <risto.kowaczewski@gmail.com>
2024-02-21 18:09:43 +0100

    feat(service): add drop method to DatabaseService
    Adds a new async method to DatabaseService class that drops a database
    if it exists. This method is used to reset installation by dropping
    the MySQL database.

--- app/api/install_endpoint.py
+++ app/api/install_endpoint.py
@@ -3,9 +3,10 @@ from fastapi import APIRouter, HTTPException
 
 from tracardi.domain.installation_status import SystemInstallationStatus
 from tracardi.service.installation import install_system
-from tracardi.config import tracardi
+from tracardi.config import tracardi, mysql
 from tracardi.domain.credentials import Credentials
 from tracardi.service.plugin.plugin_install import install_default_plugins
+from tracardi.service.storage.mysql.service.database_service import DatabaseService
 
 router = APIRouter()
 
@@ -30,4 +31,16 @@ async def install(credentials: Optional[Credentials]):
         return await install_system(credentials)
     except PermissionError as e:
         raise HTTPException(status_code=403, detail=str(e))
+
+
+@router.get("/install/reset/{token}", tags=["installation"], include_in_schema=tracardi.expose_gui_api)
+async def reset_installation(token: str):
+    """
+    Resets installation
+    """
+    if tracardi.installation_token and (tracardi.installation_token=='tracardi' or tracardi.installation_token != token):
+        raise PermissionError("Installation reset forbidden. Invalid installation token.")
+
+    db = DatabaseService()
+    return await db.drop(mysql.mysql_database)
 
