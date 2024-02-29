f301c1e8ffa1ce696f391ad56da25b2f8af9e314
risto <risto.kowaczewski@gmail.com>
2024-02-29 10:04:27 +0100

    feat(configuration): Add timestamp field to Configuration class
    feat(configuration): Add setup_configuration module
    
    feat(configuration): Add configuration endpoint functionalities

--- app/api/configuration_endpoint.py
+++ app/api/configuration_endpoint.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from app.service.grouping import get_grouped_result
 from tracardi.domain.configuration import Configuration
+from tracardi.service.setup.setup_configuration import available_configuration_list
 from tracardi.service.storage.mysql.mapping.configuration_mapping import map_to_configuration
 from tracardi.service.storage.mysql.service.configuration_service import ConfigurationService
 
@@ -16,8 +17,8 @@ router = APIRouter(
 
 cs = ConfigurationService()
 
-@router.get("/configuration/{id}", tags=["deployment"], include_in_schema=tracardi.expose_gui_api)
-async def get_test(id: str):
+@router.get("/configuration/{id}", tags=["configuration"], include_in_schema=tracardi.expose_gui_api)
+async def get_configuration(id: str):
     record = await cs.load_by_id(id)
     if not record.exists():
         raise HTTPException(status_code=404, detail=f"Configuration with ID {id} not found.")
@@ -25,21 +26,30 @@ async def get_test(id: str):
     return record.map_to_object(map_to_configuration)
 
 
-@router.get("/configuration", tags=["deployment"], include_in_schema=tracardi.expose_gui_api)
-async def list_configuration(query: Optional[str] = None, limit: int = 200):
+@router.get("/configuration", tags=["configuration"], include_in_schema=tracardi.expose_gui_api)
+async def list_defined_configuration(query: Optional[str] = None, limit: int = 200):
     records = await cs.load_all(search=query, limit=limit)
 
     return get_grouped_result("Configuration Settings", records, map_to_configuration)
 
 
-@router.post("/configuration", tags=["deployment"], include_in_schema=tracardi.expose_gui_api)
-async def add_test(config: Configuration):
+@router.post("/configuration", tags=["configuration"], include_in_schema=tracardi.expose_gui_api)
+async def add_configuration(config: Configuration):
     return await cs.upsert(config)
 
 
-@router.delete("/configuration/{id}", tags=["report"], include_in_schema=tracardi.expose_gui_api)
-async def delete_test(id: str):
+@router.delete("/configuration/{id}", tags=["configuration"], include_in_schema=tracardi.expose_gui_api)
+async def delete_configuration(id: str):
     """
     Deletes configuration from the database
     """
     return await cs.delete_by_id(id)
+
+# Lists
+
+@router.get("/configuration-type", tags=["configuration"], include_in_schema=tracardi.expose_gui_api)
+async def list_configuration_types():
+    return {
+        "total": len(available_configuration_list),
+        "result": list(available_configuration_list.values())
+    }
