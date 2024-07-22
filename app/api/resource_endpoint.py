13f5c69c045c0a0710b23d80be779922b951bad3
risto <risto.kowaczewski@gmail.com>
2024-07-22 18:10:44 +0200

    Predefined destination

--- app/api/resource_endpoint.py
+++ app/api/resource_endpoint.py
@@ -4,6 +4,8 @@ from fastapi import APIRouter, Depends
 from tracardi.domain.enum.type_enum import TypeEnum
 from tracardi.exceptions.log_handler import get_logger
 from tracardi.service.setup.setup_resources import get_type_of_resources
+from tracardi.service.storage.mysql.interface.resource import load_all_resources, load_all_resource_entities, \
+    load_resource_by_id
 from tracardi.service.storage.mysql.map_to_named_entity import map_to_named_entity
 from tracardi.service.storage.mysql.mapping.resource_mapping import map_to_resource
 from tracardi.service.storage.mysql.service.resource_service import ResourceService
@@ -71,8 +73,13 @@ async def list_resources_names_by_tag(tag: str):
             dependencies=[Depends(Permissions(roles=["admin", "developer", "marketer"]))],
             include_in_schema=tracardi.expose_gui_api)
 async def list_all_resources():
-    records = await rs.load_all(limit=250)
-    return get_result_dict(records, map_to_named_entity)
+    resources, total = await load_all_resource_entities(limit=250)
+    return {
+        "total": total,
+        "result": resources
+    }
+    # records = await rs.load_all(limit=250)
+    # return get_result_dict(records, map_to_named_entity)
 
 
 @router.get("/resources",
@@ -80,8 +87,13 @@ async def list_all_resources():
             dependencies=[Depends(Permissions(roles=["admin", "developer", "marketer"]))],
             include_in_schema=tracardi.expose_gui_api)
 async def list_resources():
-    records = await rs.load_all()
-    return get_result_dict(records, map_to_resource)
+    resources, total = await load_all_resources()
+    return {
+        "total": total,
+        "result": resources
+    }
+    # records = await rs.load_all()
+    # return get_result_dict(records, map_to_resource)
 
 
 @router.get("/resources/by_type",
@@ -89,9 +101,13 @@ async def list_resources():
             dependencies=[Depends(Permissions(roles=["admin", "developer", "marketer"]))],
             include_in_schema=tracardi.expose_gui_api)
 async def list_resources_by_type(query: str = None, limit:int = 200):
-    records = await rs.load_all(search=query, limit=limit)
-
-    return get_grouped_result("Resources", records, map_to_resource)
+    resources, total = await load_all_resources(search=query, limit=limit)
+    return {
+        "total": total,
+        "grouped": {"Resources":resources}
+    }
+    # records = await rs.load_all(search=query, limit=limit)
+    # return get_grouped_result("Resources", records, map_to_resource)
 
 
 @router.get("/resource/{id}",
@@ -103,8 +119,11 @@ async def get_resource_by_id(id: str) -> Optional[Resource]:
     """
     Returns source data with given id.
     """
-    record = await rs.load_by_id(id)
-    return record.map_to_object(map_to_resource)
+
+    return await load_resource_by_id(id)
+    #
+    # record = await rs.load_by_id(id)
+    # return record.map_to_object(map_to_resource)
 
 
 @router.post("/resource",
