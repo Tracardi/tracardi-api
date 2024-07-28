ee8f840eaf968f52ebe2e026832228d9d2721da0
risto <risto.kowaczewski@gmail.com>
2024-07-28 09:48:19 +0200

    ```markdown feat(proto): log fetched PRO services with version info ```

--- app/api/proto/tracard_pro_client.py
+++ app/api/proto/tracard_pro_client.py
@@ -72,6 +72,7 @@ class TracardiProClient(object):
             raise PermissionError(e.details())
 
     async def get_available_services(self, query="", category=""):
+        logger.info(f"Fetched PRO services fro version {tracardi.image_tag}")
         message = pb2.ServiceQuery(query=query, category=category, version=tracardi.image_tag)
 
         services = self.stub.get_available_services(message, metadata=[('token', await get_tpro_token())])
