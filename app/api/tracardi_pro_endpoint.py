879413470d550157527eaf1924e7a23f38387469
risto <risto.kowaczewski@gmail.com>
2024-09-30 09:29:46 +0200

    Removed list of services as a remote call

--- app/api/tracardi_pro_endpoint.py
+++ app/api/tracardi_pro_endpoint.py
@@ -2,6 +2,8 @@ from collections import OrderedDict
 from typing import Optional
 
 import grpc
+
+from tracardi.service.license import License
 from tracardi.service.storage.mysql.interface import resource_dao
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import ValidationError
@@ -18,6 +20,9 @@ from tracardi.exceptions.log_handler import get_logger
 from tracardi.config import tracardi
 from tracardi.service.storage.mysql.service.tracardi_pro_service import TracardiProService
 from tracardi.service.tracardi_http_client import HttpClient
+
+if License.has_license():
+    from com_tracardi.pro.service.tracardi_pro import get_available_services
 
 logger = get_logger(__name__)
 
@@ -39,89 +44,70 @@ async def is_token_valid() -> Optional[bool]:
     """
     Return None if not configured otherwise returns True if credentials are valid or False.
     """
-    try:
-        tps = TracardiProService()
-        record = await tps.load_by_tenant_id()
-
-        if not record.exists():
-            return None
-
-        token = tracardi_pro_client.validate(token=record.rows.token)
-
-        return token is not None
-
-    except ValidationError as e:
-        logger.error(f"Validation error when reading pro service user data: {str(e)}")
-        return None
-    except Exception as e:
-        logger.error(f"Exception when reading pro service user data: {str(e)}")
-        return None
+    return True
 
 
-@router.post("/tpro/sign_in", tags=["tpro"], include_in_schema=tracardi.expose_gui_api)
-async def tracardi_pro_sign_in(credentials: Credentials):
-    """
-    Handles signing in to Tracardi PRO service
-    """
-    try:
-
-        token, host = tracardi_pro_client.sign_in(credentials.username, credentials.password)
-
-        tps = TracardiProService()
-        result = await tps.authorize(token)
-
-        # Save locally if data from remote differs with local data.
-        if not result:
-            await tps.insert(token)
-
-        return True
-    except PermissionError as e:
-        msg = "Access denied due to server error:  {}".format(str(e))
-        logger.warning(msg, exc_info=True)
-        raise HTTPException(detail=msg,
-                            status_code=403)  # Must be 403 because 401 logs out gui
-    except Exception as e:
-        logger.error(str(e), exc_info=True)
-        raise HTTPException(detail=str(e), status_code=403)  # Must be 403 because 401 logs out gui
-
-
-@router.post("/tpro/sign_up", tags=["tpro"], include_in_schema=tracardi.expose_gui_api, response_model=bool)
-async def tracardi_pro_sign_up(sign_up_data: SignUpData):
-    """
-    Handles signing up to Tracardi PRO service
-    """
-    try:
-
-        token = tracardi_pro_client.sign_up(sign_up_data.username, sign_up_data.password)
-
-        tps = TracardiProService()
-        await tps.upsert(token)
-
-        return True
-
-    except grpc.RpcError as e:
-        raise HTTPException(
-            detail="Access denied due to RPC error \"{}\". Error status: {}".format(e.details(), e.code().name),
-            status_code=403)  # Must be 403 because 401 logs out gui
+# @router.post("/tpro/sign_in", tags=["tpro"], include_in_schema=tracardi.expose_gui_api)
+# async def tracardi_pro_sign_in(credentials: Credentials):
+#     """
+#     Handles signing in to Tracardi PRO service
+#     """
+#     try:
+#
+#         token, host = tracardi_pro_client.sign_in(credentials.username, credentials.password)
+#
+#         tps = TracardiProService()
+#         result = await tps.authorize(token)
+#
+#         # Save locally if data from remote differs with local data.
+#         if not result:
+#             await tps.insert(token)
+#
+#         return True
+#     except PermissionError as e:
+#         msg = "Access denied due to server error:  {}".format(str(e))
+#         logger.warning(msg, exc_info=True)
+#         raise HTTPException(detail=msg,
+#                             status_code=403)  # Must be 403 because 401 logs out gui
+#     except Exception as e:
+#         logger.error(str(e), exc_info=True)
+#         raise HTTPException(detail=str(e), status_code=403)  # Must be 403 because 401 logs out gui
+#
+#
+# @router.post("/tpro/sign_up", tags=["tpro"], include_in_schema=tracardi.expose_gui_api, response_model=bool)
+# async def tracardi_pro_sign_up(sign_up_data: SignUpData):
+#     """
+#     Handles signing up to Tracardi PRO service
+#     """
+#     try:
+#
+#         token = tracardi_pro_client.sign_up(sign_up_data.username, sign_up_data.password)
+#
+#         tps = TracardiProService()
+#         await tps.upsert(token)
+#
+#         return True
+#
+#     except grpc.RpcError as e:
+#         raise HTTPException(
+#             detail="Access denied due to RPC error \"{}\". Error status: {}".format(e.details(), e.code().name),
+#             status_code=403)  # Must be 403 because 401 logs out gui
 
 
 @router.get("/tpro/available_services", tags=["tpro"], include_in_schema=tracardi.expose_gui_api)
-async def get_available_services(query: Optional[str] = "", category: Optional[str] = ""):
+async def get_extension_services(query: Optional[str] = "", category: Optional[str] = ""):
     """
     Returns available Tracardi PRO services
     """
-    try:
-        services = await tracardi_pro_client.get_available_services(query, category)
-        if 'services' in services:
-            services['services'] = OrderedDict(sorted(services['services'].items()))
-            return services
-
+    if License.has_license():
+        services = await get_available_services(query, category, version=tracardi.image_tag if tracardi.image_tag != 'n/a' else "1.0.0")
         return {
-            "services": {}
+            "services": OrderedDict(sorted(services.items()))
         }
-    except grpc.RpcError as e:
-        # Must be 403 because 401 logs out gui
-        raise HTTPException(detail=e.details(), status_code=403 if e.code().name == 'UNAUTHENTICATED' else 500)
+
+    return {
+        "services": {}
+    }
 
 
 @router.get("/tpro/plugin/{module}", tags=["tpro"], include_in_schema=tracardi.expose_gui_api)
@@ -173,61 +159,61 @@ async def save_tracardi_pro_resource(pro: ProService):
     return result
 
 
-@router.post("/tpro/install/microservice", tags=["tpro"], include_in_schema=tracardi.expose_gui_api)
-async def save_tracardi_pro_microservice(pro: ProMicroService):
-    """
-    Adds new Tracardi PRO resource
-    """
-
-    resource = Resource.from_pro_service(pro)
-
-    def _remove_redundant_data(credentials):
-        credentials.pop('name', None)
-        credentials.pop('description', None)
-        return credentials
-
-    resource.credentials.production = _remove_redundant_data(resource.credentials.production)
-    resource.credentials.test = _remove_redundant_data(resource.credentials.test)
-
-    production_credentials = TProMicroserviceCredentials(**resource.credentials.production)
-
-    # record = ResourceRecord.encode(resource)
-    result = await _store_resource_record(resource)
-    # await resource_db.refresh()
-
-    # Create plugin
-
-    if not pro.microservice.service.id or not production_credentials.is_configured():
-        raise AssertionError("Microservice Host, Token or Service not configured")
-
-    microservice_plugin_url = f"{production_credentials.url}/plugin/registry?service_id={pro.microservice.service.id}"
-
-    async with HttpClient(3, 200, headers={
-        'Authorization': f"Bearer {production_credentials.token}"
-    }) as client:
-        async with client.get(url=microservice_plugin_url) as response:
-            data = await response.json()
-
-            if response.status != 200:
-                raise HTTPException(status_code=response.status, detail=data)
-
-            plugin = Plugin(**data)
-
-            plugin.metadata.name = resource.name
-
-            if plugin.spec.microservice is None:
-                plugin.spec.microservice = MicroserviceConfig.create()
-
-            plugin.spec.microservice.service.id = pro.microservice.service.id
-            plugin.spec.microservice.service.name = pro.microservice.service.name
-            # Set credentials, exclude service data
-            plugin.spec.microservice.server.credentials = resource.credentials
-            # Configure microservice to be connected with created resource
-            plugin.spec.microservice.server.resource.name = resource.name
-            plugin.spec.microservice.server.resource.id = resource.id
-            # Plugin resources
-            plugin.spec.microservice.plugin.resource = pro.microservice.credentials
-
-            await install_remote_plugin(plugin)
-
-    return result
+# @router.post("/tpro/install/microservice", tags=["tpro"], include_in_schema=tracardi.expose_gui_api)
+# async def save_tracardi_pro_microservice(pro: ProMicroService):
+#     """
+#     Adds new Tracardi PRO resource
+#     """
+#
+#     resource = Resource.from_pro_service(pro)
+#
+#     def _remove_redundant_data(credentials):
+#         credentials.pop('name', None)
+#         credentials.pop('description', None)
+#         return credentials
+#
+#     resource.credentials.production = _remove_redundant_data(resource.credentials.production)
+#     resource.credentials.test = _remove_redundant_data(resource.credentials.test)
+#
+#     production_credentials = TProMicroserviceCredentials(**resource.credentials.production)
+#
+#     # record = ResourceRecord.encode(resource)
+#     result = await _store_resource_record(resource)
+#     # await resource_db.refresh()
+#
+#     # Create plugin
+#
+#     if not pro.microservice.service.id or not production_credentials.is_configured():
+#         raise AssertionError("Microservice Host, Token or Service not configured")
+#
+#     microservice_plugin_url = f"{production_credentials.url}/plugin/registry?service_id={pro.microservice.service.id}"
+#
+#     async with HttpClient(3, 200, headers={
+#         'Authorization': f"Bearer {production_credentials.token}"
+#     }) as client:
+#         async with client.get(url=microservice_plugin_url) as response:
+#             data = await response.json()
+#
+#             if response.status != 200:
+#                 raise HTTPException(status_code=response.status, detail=data)
+#
+#             plugin = Plugin(**data)
+#
+#             plugin.metadata.name = resource.name
+#
+#             if plugin.spec.microservice is None:
+#                 plugin.spec.microservice = MicroserviceConfig.create()
+#
+#             plugin.spec.microservice.service.id = pro.microservice.service.id
+#             plugin.spec.microservice.service.name = pro.microservice.service.name
+#             # Set credentials, exclude service data
+#             plugin.spec.microservice.server.credentials = resource.credentials
+#             # Configure microservice to be connected with created resource
+#             plugin.spec.microservice.server.resource.name = resource.name
+#             plugin.spec.microservice.server.resource.id = resource.id
+#             # Plugin resources
+#             plugin.spec.microservice.plugin.resource = pro.microservice.credentials
+#
+#             await install_remote_plugin(plugin)
+#
+#     return result
