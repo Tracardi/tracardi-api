6b9694c08817cbc12c6b2e4adcd50b045bfe4421
risto <risto.kowaczewski@gmail.com>
2024-09-30 12:47:02 +0200

    fix: update version numbers to 1.1.x

--- build-http.sh
+++ build-http.sh
@@ -1,3 +1,3 @@
 git rev-parse HEAD > app/tracker/revision.txt
-docker build . --rm --no-cache -f docker.Dockerfile -t tracardi/tracardi-api:1.0.x-dev
-docker push tracardi/tracardi-api:1.0.x-dev
+docker build . --rm --no-cache -f docker.Dockerfile -t tracardi/tracardi-api:1.1.x
+docker push tracardi/tracardi-api:1.1.x
