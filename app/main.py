3822e170d4a31e2ea91f6c2a9920604f74666fe1
risto <risto.kowaczewski@gmail.com>
2024-12-11 12:53:24 +0100

    refactor(main.py): remove redundant multi-tenancy logs and rearrange print statements
    Simplify logging by removing redundant multi-tenancy logs.
    Rearrange license and version print statements for better clarity
    and consistency.

--- app/main.py
+++ app/main.py
@@ -345,12 +345,6 @@ async def app_starts():
             profiles_sample_rate=1.0,
         )
 
-    if License.has_service(LICENSE):
-        logger.info(f"TRACARDI multi-tenancy:  {tracardi.multi_tenant}.")
-        logger.info(f"TRACARDI multi-tenancy API:  {tracardi.multi_tenant_manager_url}.")
-
-
-
     print(f"""
     88888888888 8888888b.         d8888  .d8888b.         d8888 8888888b.  8888888b. 8888888
         888     888   Y88b       d88888 d88P  Y88b       d88888 888   Y88b 888   Y88b  888
@@ -360,23 +354,20 @@ async def app_starts():
         888     888 T88b     d88P   888 888    888   d88P   888 888 T88b   888    888  888
         888     888  T88b   d8888888888 Y88b  d88P  d8888888888 888  T88b  888   d88P  888
         888     888   T88b d88P     888  "Y8888P"  d88P     888 888   T88b 8888888P" 8888888
-
-    {str(tracardi.version)}""", flush=True)
+""", flush=True)
 
     if License.has_license():
         license = License.check()
 
-        print(
-            f"Commercial Licensed issued for: {license.owner}, expires: {datetime.fromtimestamp(license.expires) if license.expires > 0 else 'Perpetual'} ",
-            flush=True)
-
-        print(f"Services {list(license.get_service_ids())}", flush=True)
+        print(f"{str(tracardi.version)} (Multi-Tenant: {tracardi.multi_tenant}) (Adapters: {tracardi.cache_adapter},{com_tracardi_settings.queue_adapter})", flush=True)
+        print(f"Commercial Licensed issued for: {license.owner}, expires: {datetime.fromtimestamp(license.expires) if license.expires > 0 else 'Perpetual'} ", flush=True)
+        print(f"TRACARDI Services {list(license.get_service_ids())}", flush=True)
+        print(f"TRACARDI multi-tenancy API:  {tracardi.multi_tenant_manager_url}.", flush=True)
     else:
+        print(f"{str(tracardi.version)}(Multi - Tenant: {tracardi.multi_tenant}", flush=True)
         print("License: MIT + “Commons Clause” License Condition v1.0", flush=True)
 
-    logger.info(f"Adapters: CACHE={tracardi.cache_adapter}")
-    if License.has_license():
-        logger.info(f"Adapters: QUEUE={com_tracardi_settings.queue_adapter}")
+
 
     if tracardi.enable_global_settings:
         bs = GlobalSettingsBroadcaster()
