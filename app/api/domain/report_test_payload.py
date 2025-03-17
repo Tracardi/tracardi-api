d0161f3b58e9196a3830f9edee6c4ec2da405c15
risto <risto.kowaczewski@gmail.com>
2025-03-17 13:17:10 +0100

    Commit deleted

--- app/api/domain/report_test_payload.py
+++
@@ -1,8 +0,0 @@
-from tracardi.domain.report import Report
-from pydantic import BaseModel
-from typing import Dict, Any
-
-
-class ReportTestPayload(BaseModel):
-    report: Report
-    params: Dict[str, Any]
--- app/api/domain/rss.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from pydantic import BaseModel
-
-
-class RssItem(BaseModel):
-    title: str
-    description: str
-    link: str
-    publish_date: datetime
